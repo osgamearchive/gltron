f415677c9d4a17c0f42b7b49b75d0dfda8b20f17
Andreas Umbach <umbach@gmail.com>
2000-07-19 21:12:25 +0000

    - win32 will use data/music.list intead of opendir / readdir   to determine song list (TODO: fix that)

--- directory.c
+++ directory.c
@@ -1,9 +1,46 @@
-#include <sys/types.h>
-#include <dirent.h>
 #include <stdio.h>
-
 #include "gltron.h"
 
+#ifdef WIN32
+list *readDirectoryContents(char *dirname, char *prefix) {
+  char *path;
+  FILE *f;
+  char buf[100];
+  list *l, *p;
+
+  path = getFullPath("music.list");
+  if(path == NULL) {
+    fprintf(stderr, "can't locate music.list, aborting\n");
+    exit(2);
+  }
+  if((f = fopen(path, "r")) == NULL) {
+    fprintf(stderr, "can't open music.list, aborting\n");
+    exit(2);
+  }
+    
+  free(path);
+
+  l = (list*) malloc(sizeof(list));
+  p = l;
+  p->next = NULL;
+
+  while(fgets(buf, sizeof(buf), f)) {
+    char *name;
+    name = malloc(strlen(buf));
+    buf[ strlen(buf) - 1 ] = 0;
+    memcpy(name, buf, strlen(buf) + 1);
+    fprintf(stderr, "adding song '%s'\n", name);
+    p->data = name;
+    p->next = (list*) malloc(sizeof(list));
+    p = p->next;
+    p->next = NULL;
+  }
+  return l;
+}
+#else
+
+#include <sys/types.h>
+#include <dirent.h>
 list* readDirectoryContents(char *dirname, char *prefix) {
   DIR *dir;
   struct dirent *entry;
@@ -29,4 +66,4 @@ list* readDirectoryContents(char *dirname, char *prefix) {
   return l;
 
 }
-
+#endif
