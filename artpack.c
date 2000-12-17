2398744b53d7254493a229d5ddfbbeacf09d979c
Andreas Umbach <umbach@gmail.com>
2000-12-17 23:15:17 +0000

    - contains some of the ugly art-pack code

---
+++ artpack.c
@@ -0,0 +1,36 @@
+#include "gltron.h"
+
+char **artpack_list;
+int artpack_index = 0;
+
+void initArtpacks() {
+  list *l, *p;
+  int i = 0, n = 0;
+
+  l = readDirectoryContents(PATH_ART, NULL);
+  for(p = l; p->next != NULL; p = p->next)
+    n++;
+
+  artpack_list = (char**) malloc((n + 1) * sizeof(char*));
+  
+  for(p = l; p->next != NULL; p = p->next) {
+    artpack_list[i] = (char*) malloc(strlen( (char*) p->data ) + 1);
+    memcpy(artpack_list[i], p->data, strlen( (char*) p->data ) + 1);
+    printf("loading artpack %s\n", artpack_list[i]);
+    if(strstr(artpack_list[i], "default") == artpack_list[i])
+      artpack_index = i;
+    i++;
+
+  }
+  printf("loaded %d artpacks\n", i);
+  artpack_list[i] = NULL;
+}
+
+void reloadArt() {
+  printf("reloading art\n");
+  deleteTextures(game->screen);
+  game->screen->artpack.path = artpack_list[artpack_index];
+  initTexture(game->screen);
+}
+    
+
