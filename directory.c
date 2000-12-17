f16b25e75445657ff3865a18d43a58d707367c3b
Andreas Umbach <umbach@gmail.com>
2000-12-17 23:20:45 +0000

    - removed . and .. from directory listings (Unix)

--- directory.c
+++ directory.c
@@ -189,12 +189,14 @@ list* readDirectoryContents(char *dirname, char *prefix) {
   while((entry = readdir(dir)) != NULL) {
     char *name;
     if(prefix == NULL || strstr(entry->d_name, prefix) == entry->d_name) {
-      name = malloc(strlen(entry->d_name) + 1);
-      memcpy(name, entry->d_name, strlen(entry->d_name) + 1);
-      p->data = name;
-      p->next = (list*) malloc(sizeof(list));
-      p = p->next;
-      p->next = NULL;
+      if(entry->d_name[0] != '.') {
+	name = malloc(strlen(entry->d_name) + 1);
+	memcpy(name, entry->d_name, strlen(entry->d_name) + 1);
+	p->data = name;
+	p->next = (list*) malloc(sizeof(list));
+	p = p->next;
+	p->next = NULL;
+      }
     }
   }
   closedir(dir);
