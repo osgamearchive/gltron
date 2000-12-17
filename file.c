78dd7cf23776a62bccf80f0a2744b8a7dec8133e
Andreas Umbach <umbach@gmail.com>
2000-12-17 23:26:33 +0000

    - slimmed down code, support multiple search directories

--- file.c
+++ file.c
@@ -14,99 +14,31 @@
 #    define SEPERATOR '/'
 #endif
 
-/* #ifdef UNIX */
-static char *share1 = "/usr/share/games/gltron";
-static char *share2 = "/usr/local/share/games/gltron";
-/* #endif */
+static int n_dirs = 2;
 
 #ifdef macintosh 
-static char *subdir = ":Data";
+static char *dirs[] = { ":Data", "art" };
 #else
-static char* subdir = "data";
+static char *dirs[] = { "data", "art" };
 #endif
-
-#if defined WIN32 || defined macintosh 
-/* assume current directory + data/ */
-char* getFullPath(char *filename) {
-  char *path;
-  fprintf(stderr, "using win32/mac hack looking for %s\n", filename);
-  path = malloc(strlen(subdir) + 1 + strlen(filename) + 1);
-  sprintf(path, "%s%c%s", subdir, SEPERATOR, filename);
-
-  return path;
-}
-#else
 
 char* getFullPath(char *filename) {
   char *path;
   FILE *fp = NULL;
-  char *base;
 
-  /* check a few directories for the files and */
-  /* return the full path. */
-  
-  /* check: data sub-directory, GLTRON_HOME, and */
-  /* /usr/share/games/gltron and /usr/local/share/games/gltron */
-
-  path = malloc(strlen(subdir) + 1 + strlen(filename) + 1);
-  sprintf(path, "%s%c%s", subdir, SEPERATOR, filename);
-
-  printf("checking '%s'...", path);
-  fp = fopen(path, "r");
-  if(fp != NULL) {
-    fclose(fp);
-	printf("ok\n");
-    return path;
-  }
-  free(path);
-  printf("unsuccessful\n");
-
-  base = getenv("GLTRON_HOME");
-  if(base != NULL) {
-    path = malloc(strlen(base) + 1 + strlen(filename) + 1);
-    sprintf(path, "%s%c%s", base, SEPERATOR, filename);
-
+  int i;
+  for(i = 0; i < n_dirs; i++) {
+    path = malloc(strlen(dirs[i]) + 1 + strlen(filename) + 1);
+    sprintf(path, "%s%c%s", dirs[i], SEPERATOR, filename);
     printf("checking '%s'...", path);
     fp = fopen(path, "r");
     if(fp != NULL) {
       fclose(fp);
-	  printf("ok\n");
+      printf("ok\n");
       return path;
     }
     free(path);
     printf("unsuccessful\n");
   }
-
-  path = malloc(strlen(share1) + 1 + strlen(filename) + 1);
-  sprintf(path, "%s%c%s", share1, SEPERATOR, filename);
-
-  printf("checking '%s'", path);
-  fp = fopen(path, "r");
-  if(fp != NULL) {
-	printf("ok\n");
-    fclose(fp);
-    return path;
-  }
-  free(path);
-  printf("unsuccessful\n");
-
-  path = malloc(strlen(share2) + 1 + strlen(filename) + 1);
-  sprintf(path, "%s%c%s", share2, SEPERATOR, filename);
-  
-  printf("checking '%s'", path);
-  fp = fopen(path, "r");
-  if(fp != NULL) {
-    fclose(fp);
-	printf("ok\n");
-    return path;
-  }  
-  free(path);
-  printf("unsuccessful\n");
-
   return NULL;
 }
-
-#endif  
-  
-
-  
