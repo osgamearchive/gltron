83872663ed10204270aac2befdba8ca654418a3c
Andreas Umbach <umbach@gmail.com>
2000-12-18 00:52:44 +0000

    - header file for the new texture code

---
+++ texture.h
@@ -0,0 +1,27 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+typedef struct {
+  char name[48];
+  int type;
+  int wrap_s;
+  int wrap_t;
+  int id;
+} texture_info;
+
+enum {
+  TEX_FLOOR = 0,
+  TEX_GUI,
+  TEX_LOGO,
+  TEX_WALL1,
+  TEX_WALL2,
+  TEX_WALL3,
+  TEX_WALL4,
+  TEX_CRASH,
+  TEX_TRAIL,
+  TEX_DECAL,
+  TEX_DEBUG
+};
+
+#endif
+
