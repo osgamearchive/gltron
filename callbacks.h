2e89bf44b6d1ccba7afd62a5c086afa177d45e31
Andreas Umbach <umbach@gmail.com>
2001-02-03 12:54:22 +0000

    - adding callbacks.h

---
+++ callbacks.h
@@ -0,0 +1,13 @@
+#ifndef CALLBACKS_H
+#define CALLBACKS_H
+typedef struct callbacks {
+  void (*display)(void);
+  void (*idle)(void);
+  void (*keyboard)(int, int, int);
+  void (*init)(void);
+  void (*exit)(void);
+  void (*initGL)(void);
+  void (*mouse)(int, int, int, int);
+  void (*mouseMotion)(int, int);
+} callbacks;
+#endif
