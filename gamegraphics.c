a0fe2a853bf68c9a04a3b92789cac7398766fe5f
Andreas Umbach <umbach@gmail.com>
2001-02-03 12:42:12 +0000

    - added replacements for gluPerspective and gluLookAt

--- gamegraphics.c
+++ gamegraphics.c
@@ -702,9 +702,43 @@ void drawWalls(gDisplay *d) {
   glDisable(GL_CULL_FACE);
 }
 
+
+void doPerspective(float fov, float ratio, float znear, float zfar) {
+  float top;
+  float left;
+
+  top = tan( fov * M_PI / 360.0 ) * znear;
+  left = - top * ratio;
+  glFrustum(left, -left, -top, top, znear, zfar);
+}
+
+void doLookAt(float *cam, float *target, float *up) {
+  float m[16];
+  float x[3], y[3], z[3];
+
+  vsub(cam, target, z);
+  normalize(z);
+  crossprod(up, z, x);
+  crossprod(z, x, y);
+  normalize(x);
+  normalize(y);
+
+#define M(row,col)  m[col*4+row]
+   M(0,0) = x[0];  M(0,1) = x[1];  M(0,2) = x[2];  M(0,3) = 0.0;
+   M(1,0) = y[0];  M(1,1) = y[1];  M(1,2) = y[2];  M(1,3) = 0.0;
+   M(2,0) = z[0];  M(2,1) = z[1];  M(2,2) = z[2];  M(2,3) = 0.0;
+   M(3,0) = 0.0;   M(3,1) = 0.0;   M(3,2) = 0.0;   M(3,3) = 1.0;
+#undef M
+   glMultMatrixf( m );
+
+   /* Translate Eye to Origin */
+   glTranslatef( -cam[0], -cam[1], -cam[2]);
+}
+
+
 void drawCam(Player *p, gDisplay *d) {
   int i;
-
+  float up[3] = { 0, 0, 1 };
   /* 
   if (d->fog == 1) {
     glEnable(GL_FOG);
@@ -715,16 +749,16 @@ void drawCam(Player *p, gDisplay *d) {
   glColor3f(0.0, 1.0, 0.0);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(game->settings->fov, d->vp_w / d->vp_h,
-		 1.0, game->settings->grid_size * 6.0);
+  doPerspective(game->settings->fov, d->vp_w / d->vp_h,
+		 game->settings->znear, game->settings->grid_size * 6.5);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   /* set positions for GL lights in world coordinates */
   glLightfv(GL_LIGHT1, GL_POSITION, p->camera->cam);
-  gluLookAt(p->camera->cam[0], p->camera->cam[1], p->camera->cam[2],
-	    p->camera->target[0], p->camera->target[1], p->camera->target[2],
-	    0, 0, 1);
+
+  doLookAt(p->camera->cam, p->camera->target, up);
+
   glDisable(GL_LIGHTING);
 
   if(game->settings->show_skybox)
