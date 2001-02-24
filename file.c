5f83aa1a325df72e4380ecd87e01e146051e17a1
walisser <walisser@962c35fc-89ae-4f3e-9c41-567b7e30456f>
2001-02-24 15:01:22 +0000

    fopen() only works for files on MacOS, but we want to get the full path to directories too. So I created the itemExists() function which uses access() on POSIX systems and a custom function for MacOS.

--- file.c
+++ file.c
@@ -1,6 +1,7 @@
 // #include "gltron.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #ifdef macintosh
 #    define SEPERATOR ':'
@@ -17,10 +18,54 @@
 static int n_dirs = 2;
 
 #ifdef macintosh 
-static char *dirs[] = { ":Data", "art" };
+static char *dirs[] = { ":Data", ":Art" };
 #else
 static char *dirs[] = { "data", "art" };
 #endif
+
+/* fopen() in MacOS on a directory returns NULL, here is a better solution */
+/* that tells us if a file OR directory exists */
+#ifdef macintosh
+
+#include <Files.h>
+
+extern OSStatus GetApplicationDirectory(short *vRefNum, long *dirID); /* in directory.c */
+
+static int itemExists ( const char* path ) {
+    
+  OSStatus  err;
+  Str255    relPath; 
+  short     vRefNum;
+  long      dirID;
+  FSSpec    spec;
+
+  if (*path != ':')
+   sprintf (relPath, "%c:%s", (strlen(path) + 1), path);
+  else 
+   sprintf (relPath, "%c%s", (strlen(path)) , path);
+
+  err = GetApplicationDirectory (&vRefNum, &dirID);
+  if (err != noErr) {
+    fprintf (stderr, "GetApplicationDirectory failed\n");
+    exit (-1);
+  }
+  
+  err = FSMakeFSSpec  (vRefNum, dirID, relPath, &spec);
+  
+  return (err == noErr);
+}
+
+#else /* UNIX, Windoze, MacOS X */
+
+#include <unistd.h>
+
+static int itemExists (const char* path) {
+
+    return (access (path, F_OK) == 0);
+}
+
+#endif
+
 
 char* getFullPath(char *filename) {
   char *path;
@@ -31,9 +76,7 @@ char* getFullPath(char *filename) {
     path = malloc(strlen(dirs[i]) + 1 + strlen(filename) + 1);
     sprintf(path, "%s%c%s", dirs[i], SEPERATOR, filename);
     printf("checking '%s'...", path);
-    fp = fopen(path, "r");
-    if(fp != NULL) {
-      fclose(fp);
+    if (itemExists (path) ) {
       printf("ok\n");
       return path;
     }
