4ffac1d7c6a69da20df5422ff8266b193813284b
Andreas Umbach <umbach@gmail.com>
2001-04-06 22:38:24 +0000

    - new camera movement - very experimental gamma stuff - F10 only changes player camera - new mouse pointer warping

--- pause.c
+++ pause.c
@@ -2,6 +2,8 @@
 
 /* very brief - just the pause mode */
 
+// static float Gamma = 2.0;
+
 void idlePause() {
 #ifdef SOUND
   soundIdle();
@@ -10,7 +12,7 @@ void idlePause() {
   if(SystemGetElapsedTime() - lasttime < 10) return;
   timediff();
   */
-  chaseCamMove();
+  doCameraMovement();
   if(game->settings->screenSaver && stoptime != 0 &&
      SystemGetElapsedTime() - stoptime > 2000) {
     initData();
@@ -46,11 +48,28 @@ void keyboardPause(int key, int x, int y) {
   case SYSTEM_KEY_F2: defaultDisplay(1); break;
   case SYSTEM_KEY_F3: defaultDisplay(2); break;
   case SYSTEM_KEY_F4: defaultDisplay(3); break;
-
+    /* doesn't really work on my box */
+    /*
+  case SYSTEM_KEY_F6: 
+    Gamma -= 0.1;
+    shutdownDisplay(game->screen);
+    SystemSetGamma(Gamma, Gamma, Gamma);
+    setupDisplay(game->screen);
+    break;
+  case SYSTEM_KEY_F7: 
+    Gamma += 0.1;
+    shutdownDisplay(game->screen);
+    SystemSetGamma(Gamma, Gamma, Gamma);
+    setupDisplay(game->screen);
+    break;
+    */
   case SYSTEM_KEY_F10:
     game->settings->camType = (game->settings->camType + 1) % CAM_COUNT;
     for(i = 0; i < game->players; i++)
-      game->player[i].camera->camType = game->settings->camType;
+      if(game->player[i].ai->active == 0)
+	initCamera(game->player[i].camera, 
+		   game->player[i].data, 
+		   game->settings->camType);
     break;
   case SYSTEM_KEY_F12: doScreenShot(game->screen->vp_w, game->screen->vp_h); break;
     
@@ -63,10 +82,13 @@ void keyboardPause(int key, int x, int y) {
 }
 
 void initPause() {
+  SystemWarpPointer(MOUSE_ORIG_X, MOUSE_ORIG_Y);
   fprintf(stderr, "init pause mode\n");
+  // Mix_SetPostMix(playEngine, NULL);
 }
 
 void exitPause() {
+  // Mix_SetPostMix(NULL, NULL);
 }
 
 void initPauseGL() {
