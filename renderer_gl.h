022761a699e2187807aff85f0d64f970e40c34bb
Andreas Umbach <umbach@gmail.com>
2001-04-06 22:40:07 +0000

    - new constants - renderTris prototype

--- renderer_gl.h
+++ renderer_gl.h
@@ -8,15 +8,24 @@
 
 #define POLY_COUNT
 
+#define RENDERER_TYPE_ALL 255
+#define RENDERER_TYPE_COLOR 1
+#define RENDERER_TYPE_NORMAL 2
+#define RENDERER_TYPE_TEXTURE_COORD 4
+#define RENDERER_TYPE_TEXTURE 8
+#define RENDERER_TYPE_TEXTURE_MODE 16
+
 extern void printRendererInfo();
 extern void renderQuad(Quad *q);
+extern void renderTris(Tris *q);
 extern void clearState();
 
 typedef struct GLstate {
-  int tex_id;
-  int tex_env_mode;
-  int binds;
-  int mod_changes;
+  int tex_id; /* current texture */
+  int tex_env_mode; /* current texture env mode */
+  int binds; /* texture bind changes - used for statistics only */
+  int mod_changes;   /* mode changes - used for statistics only */
+  int type_mask; /* which modes to change */
 } GLstate;
 
 extern GLstate *state;
