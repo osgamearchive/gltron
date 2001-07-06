23cd4ebaacf06c802906decedc32ff2fb3ecd952
sem76 <sem76@962c35fc-89ae-4f3e-9c41-567b7e30456f>
2001-07-06 11:05:00 +0000

    *** empty log message ***

---
+++ wtypes.h
@@ -0,0 +1,20 @@
+#ifndef __WTYPES_H__
+#define __WTYPES_H__
+
+
+
+/** define a rect */
+typedef struct Rect {
+  short       top;
+  short       left;
+  short       bottom;
+  short       right;
+} Wrect;
+
+/** define a cell */
+typedef struct Point {
+  short      v;
+  short      h;
+} Wpoint;
+
+#endif
