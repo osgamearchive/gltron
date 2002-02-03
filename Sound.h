9a3a033b61eaa74810515f05a131a5e77501c638
walisser <walisser@962c35fc-89ae-4f3e-9c41-567b7e30456f>
2002-02-03 23:02:45 +0000

    -more macos build fixes -some changes for compiling C in C++ mode (not complete) -removed obsolete files

--- Sound.h
+++ Sound.h
@@ -5,7 +5,16 @@
 #include "SDL_sound.h"
 
 #include <zlib.h>
-
 #include <assert.h>
+
+extern void loadFX();
+extern int  initSound();
+extern void shutdownSound();
+extern void loadSound(char *name);
+extern void playSound();
+extern void stopSound();
+extern void soundIdle();
+extern void setMusicVolume(float volume);
+extern void setFxVolume(float volume);
 
 #endif
