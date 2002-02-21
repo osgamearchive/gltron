e49a516aa9c03a3ef7b99a3b0cd260be6b2b7e9c
Andreas Umbach <umbach@gmail.com>
2002-02-21 13:34:15 +0000

    - temporary config file, incomplete

---
+++ config.lua
@@ -0,0 +1,65 @@
+show_gl_logo = 1
+play_fx = 1
+playMusic = 1
+grid_size = 720
+windowMode = 1
+
+-- This stuff is commented out, will add later
+-- iset show_help 0
+-- iset show_fps 1
+-- iset show_wall 1
+-- iset show_glow 1
+-- iset show_2d 0
+-- iset show_alpha 1
+-- iset show_floor_texture 1
+-- iset line_spacing 20
+-- iset erase_crashed 1
+-- iset fast_finish 1
+-- iset fov 105
+-- iset width 800
+-- iset height 600
+-- iset show_ai_status 0
+-- iset camType 1
+-- iset display_type 0
+-- iset show_model 1
+-- iset ai_player1 0
+-- iset ai_player2 1
+-- iset ai_player3 1
+-- iset ai_player4 1
+-- iset show_crash_texture 1
+-- iset turn_cycle 1
+-- iset mouse_warp 0
+-- iset windowMode 1
+-- iset model_backwards 0
+-- iset use_mipmaps 1
+-- iset game_speed 3
+-- iset arena_size 3
+-- iset alpha_trails 1
+-- iset lod 0
+-- iset ai_level 1
+-- iset mipmap_filter 1
+-- iset stretch_textures 0
+-- iset show_skybox 1
+-- iset bitdepth_32 0
+-- iset show_decals 1
+-- iset show_scores 1
+-- iset shadow_lod 0
+-- iset show_gl_logo 1
+-- iset show_recognizer 1
+-- fset speed 12.00
+-- fset musicVolume 0.30
+-- fset fxVolume 0.70
+-- vset keys 97 115 107 108 53 54 276 275 
+-- vset cycle_colors_diffuse 0 1.000 1.000 0.000 1.000
+-- vset cycle_colors_diffuse 1 0.700 0.100 0.100 1.000
+-- vset cycle_colors_diffuse 2 0.300 1.000 0.800 1.000
+-- vset cycle_colors_diffuse 3 0.800 0.800 0.800 1.000
+-- vset cycle_colors_specular 0 1.000 1.000 1.000 1.000
+-- vset cycle_colors_specular 1 1.000 1.000 1.000 1.000
+-- vset cycle_colors_specular 2 1.000 1.000 1.000 1.000
+-- vset cycle_colors_specular 3 1.000 1.000 1.000 1.000
+-- vset trail_colors 0 0.600 0.600 0.000 0.600
+-- vset trail_colors 1 0.500 0.050 0.250 0.600
+-- vset trail_colors 2 0.200 0.600 0.400 0.600
+-- vset trail_colors 3 0.700 0.700 0.700 0.600
+-- vset cam_settings circle 45.000 1.047 520.438 follow 45.000 0.776 12.356 cockpit 3.000 0.000 0.000 free 17.000 1.047 0.000 
