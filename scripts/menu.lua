21e2ee5e1cfca351a39986b1af41d49a041d932e
Andreas Umbach <umbach@gmail.com>
2002-02-26 08:48:54 +0000

    added some whitespace to show menu hierarchy

--- scripts/menu.lua
+++ scripts/menu.lua
@@ -8,18 +8,18 @@ Menu.type.action = 6
 -- Menu definitions
 RootMenu = { type = Menu.type.menu, caption = "" }
 GameMenu = { type = Menu.type.menu, caption = "Game" }
-GameRulesMenu = { type = Menu.type.action, caption = "Game Rules" }
-GameSettingsMenu = { type = Menu.type.action, caption = "Play Settings" }
-PlayerConfigMenu = { type = Menu.type.action, caption = "Configure Players" }
-KeyConfigMenu = { type = Menu.type.action, caption = "Configure Keys" }
-Player1_KeyMenu = { type = Menu.type.action, caption = "Player 1" }
-Player2_KeyMenu = { type = Menu.type.action, caption = "Player 2" }
-Player3_KeyMenu = { type = Menu.type.action, caption = "Player 3" }
-Player4_KeyMenu = { type = Menu.type.action, caption = "Player 4" }
+  GameRulesMenu = { type = Menu.type.action, caption = "Game Rules" }
+  GameSettingsMenu = { type = Menu.type.action, caption = "Play Settings" }
+  PlayerConfigMenu = { type = Menu.type.action, caption = "Configure Players" }
+  KeyConfigMenu = { type = Menu.type.action, caption = "Configure Keys" }
+    Player1_KeyMenu = { type = Menu.type.action, caption = "Player 1" }
+    Player2_KeyMenu = { type = Menu.type.action, caption = "Player 2" }
+    Player3_KeyMenu = { type = Menu.type.action, caption = "Player 3" }
+    Player4_KeyMenu = { type = Menu.type.action, caption = "Player 4" }
 VideoMenu = { type = Menu.type.action, caption = "Video" }
-TextureMenu = { type = Menu.type.action, caption = "Texture Options" }
-DetailsMenu = { type = Menu.type.action, caption = "Detail Options" }
-ScreenMenu = { type = Menu.type.action, caption = "Screen Options" }
+  TextureMenu = { type = Menu.type.action, caption = "Texture Options" }
+  DetailsMenu = { type = Menu.type.action, caption = "Detail Options" }
+  ScreenMenu = { type = Menu.type.action, caption = "Screen Options" }
 AudioMenu = { type = Menu.type.action, caption = "Audio" }
 
 -- Item definitions
@@ -55,28 +55,29 @@ RootMenu.items = { GameMenu, VideoMenu, AudioMenu, Quit }
 GameMenu.items = { 
    StartGame, GameRulesMenu, GameSettingsMenu, PlayerConfigMenu, KeyConfigMenu
 }
-GameRulesMenu.items = { GameSpeed, BotSkill, ArenaSize, EraseDeadPlayers }
-GameSettingsMenu.items = { FastFinish, CameraMode, Viewports, ResetScores }
-PlayerConfigMenu.items = { Player1, Player2, Player3, Player4 }
-KeyConfig.items = { 
-   Player1_KeyMenu, Player2_KeyMenu, Player3_KeyMenu,Player4_KeyMenu
-}
-Player1_KeyMenu.items = { Player1_Left, Player1_Right }
-Player2_KeyMenu.items { Player2_Left, Player2_Right }
-Player3_KeyMenu.items { Player3_Left, Player3_Right }
-Player4_KeyMenu.items { Player4_Left, Player4_Right }
+  GameRulesMenu.items = { GameSpeed, BotSkill, ArenaSize, EraseDeadPlayers }
+  GameSettingsMenu.items = { FastFinish, CameraMode, Viewports, ResetScores }
+  PlayerConfigMenu.items = { Player1, Player2, Player3, Player4 }
+  KeyConfig.items = { 
+    Player1_KeyMenu, Player2_KeyMenu, Player3_KeyMenu,Player4_KeyMenu
+  }
+    Player1_KeyMenu.items = { Player1_Left, Player1_Right }
+    Player2_KeyMenu.items { Player2_Left, Player2_Right }
+    Player3_KeyMenu.items { Player3_Left, Player3_Right }
+    Player4_KeyMenu.items { Player4_Left, Player4_Right }
 
 VideoMenu.items = { TextureMenu, DetailsMenu, ScreenMenu }
-TextureMenu.items = { 
-   Artpack, Floor, Wall, Crash, Skybox, Decals, Mipmaps, Filtering, WallScaling
-}
-DetailMenu.items = {
-   AlphaTrails, Halos, Lightcycles, Recognizer, CycleLod, ShadowLod, 
-   FPS_Counter, AI_Status, Scores
-}
-ScreenMenu.items = {
-   _320x240, _512x384, _640x480, _800x600, _1024x768, _1280x1024,
-   _1600x1200, WindowMode
-}
+  TextureMenu.items = { 
+    Artpack, Floor, Wall, Crash, Skybox, 
+    Decals, Mipmaps, Filtering, WallScaling
+  }
+  DetailMenu.items = {
+    AlphaTrails, Halos, Lightcycles, Recognizer, CycleLod, ShadowLod, 
+    FPS_Counter, AI_Status, Scores
+  }
+  ScreenMenu.items = {
+    _320x240, _512x384, _640x480, _800x600, _1024x768, _1280x1024,
+    _1600x1200, WindowMode
+  }
 
 AudioMenu.items = { Music, FX, Music_Volume, FX_Volume, Song }
