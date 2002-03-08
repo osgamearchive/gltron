b5d4ea48b9948282f8b2d92b169f180c7b2f6d0d
jcatki <jcatki@962c35fc-89ae-4f3e-9c41-567b7e30456f>
2002-03-08 10:17:36 +0000

    reverse line and filled poly order in recognizer, for smooth line blending to look correct

--- recognizer.c
+++ recognizer.c
@@ -64,16 +64,6 @@ void drawRecognizers(int flag) {
   glScalef(0.25, 0.25, 0.25);
 
   if (flag) {
-    glEnable(GL_POLYGON_OFFSET_LINE);
-    glPolygonOffset(-1.0, -1.0);
-    glColor3fv(rec_outline_color);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glEnable(GL_LINE_SMOOTH);
-    drawQuadModel(recognizer_quad);
-    glDisable(GL_LINE_SMOOTH);
-    glDisable(GL_POLYGON_OFFSET_LINE);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
- 
     glDisable(GL_LIGHT0);
     glDisable(GL_LIGHT1);
     glLightfv(GL_LIGHT2, GL_SPECULAR, rec_spec_color); 
@@ -82,15 +72,33 @@ void drawRecognizers(int flag) {
     glDisable(GL_BLEND);
     if (getSettingi("light_cycles"))
       glEnable(GL_LIGHTING);
+
+    glEnable(GL_POLYGON_OFFSET_FILL);
+    glPolygonOffset(1,1);
   }
- 
+
   glEnable(GL_NORMALIZE);
   glColor3f(0.0, 0.0, 0.0);
   drawModel(recognizer);
  
-  glDisable(GL_LIGHT2);
-  glEnable(GL_LIGHT1);
-  glDisable(GL_LIGHTING);
+  if (flag) {
+    glDisable(GL_POLYGON_OFFSET_FILL);
+
+    glDisable(GL_LIGHT2);
+    glEnable(GL_LIGHT1);
+    glDisable(GL_LIGHTING);
+
+    /*glEnable(GL_POLYGON_OFFSET_LINE);
+    glPolygonOffset(-1.0, -1.0);*/
+    glColor3fv(rec_outline_color);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glEnable(GL_BLEND);
+    glEnable(GL_LINE_SMOOTH);
+    drawQuadModel(recognizer_quad);
+    glDisable(GL_LINE_SMOOTH);
+    /*glDisable(GL_POLYGON_OFFSET_LINE);*/
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  }
 
   glPopMatrix();
 }  
