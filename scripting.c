c83ac847a3254ca7c6247e97d447461fd163d21b
Andreas Umbach <umbach@gmail.com>
2002-03-22 09:07:26 +0000

    - new functions: GetIntegerResult / GetStringResult, RunGC (garbage collector) - CopyStringResult doesn't allocate memory

--- scripting.c
+++ scripting.c
@@ -1,17 +1,25 @@
+#include "scripting.h"
+
 #include "lua.h"
 #include "lualib.h"
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static lua_State *L;
+// static lua_State *L;
+lua_State *L;
+
+extern void init_c_interface(lua_State *L);
 
 void scripting_Init() {
   L = lua_open(0);
   lua_baselibopen(L);
   lua_strlibopen(L);
   lua_iolibopen(L);
+
+  init_c_interface(L);
 }
 
 void scripting_Quit() {
@@ -33,17 +41,22 @@ int scripting_GetFloat(char *name, float *f) {
 
 
 int scripting_GetInteger(char *name, int *i) {
-  int status;
   lua_getglobal(L, name);
-  if(lua_isnumber(L, 1)) {
+  return scripting_GetIntegerResult(i);
+}
+
+int scripting_GetIntegerResult(int *i) {
+  int status;
+  int top = lua_gettop(L);
+   if(lua_isnumber(L, top)) {
     status = 0; /* all's well */
-    *i = (int)lua_tonumber(L, 1);
+    *i = (int)lua_tonumber(L, top);
   } else
     status = 1;
 
   lua_pop(L, 1); /* restore stack */
   return status;
-}
+}  
 
 void scripting_SetFloat(char *name, float f) {
   lua_pushnumber(L, f);
@@ -66,15 +79,20 @@ void scripting_GetFloatArray(char *name, float *f, int n) {
 }
 
 int scripting_GetString(char *name, char **s) {
-  int status;
-
   lua_getglobal(L, name);
-  if(lua_isstring(L, 1)) {
+  return scripting_GetStringResult(s);
+}
+
+int scripting_GetStringResult(char **s) {
+  int status;
+  int top = lua_gettop(L);
+  if(lua_isstring(L, top)) {
     int size;
     status = 0;
-    size = lua_strlen(L, 1);
+    size = lua_strlen(L, top) + 1;
     *s = malloc( size );
-    memcpy( *s, lua_tostring(L, 1), size );
+    memcpy( *s, lua_tostring(L, top), size );
+    /* printf("allocated string '%s' of size %d\n", *s, size); */
   } else
     status = 1;
 
@@ -82,10 +100,41 @@ int scripting_GetString(char *name, char **s) {
   return status;
 }
 
-void scripting_DoFile(char *name) {
+int scripting_CopyStringResult(char *s, int len) {
+  int status;
+  int top = lua_gettop(L);
+  if(lua_isstring(L, top)) {
+    int size, copy;
+    status = 0;
+    size = lua_strlen(L, top) + 1;
+    if(size > len) { copy = len; status = 2; }
+    else copy = size;
+    memcpy( s, lua_tostring(L, top), size );
+  } else
+    status = 1;
+
+  lua_pop(L, 1);
+  return status;
+}    
+
+void scripting_RunFile(char *name) {
   lua_dofile(L, name);
 }
 
-void scripting_DoString(char *command) {
+void scripting_Run(char *command) {
+  /* fprintf(stderr, "[command] %s\n", command); */
   lua_dostring(L, command);
+}
+
+void scripting_RunFormat(char *format, ... ) {
+  char buf[4096];
+  va_list ap;
+  va_start(ap, format);
+  vsprintf(buf, format, ap);
+  va_end(ap);
+  scripting_Run(buf);
+}
+
+void scripting_RunGC() {
+  lua_setgcthreshold(L, 0);
 }
--- scripting.h
+++ scripting.h
@@ -3,11 +3,14 @@ extern void scripting_Quit();
 
 extern int scripting_GetFloat(char *name, float *f);
 extern int scripting_GetInteger(char *name, int *i);
+extern int scripting_GetIntegerResult(int *i);
 extern void scripting_GetFloatArray(char *name, float *f, int n);
 extern int scripting_GetString(char *name, char **s);
+extern int scripting_GetStringResult(char **s);
+extern int scripting_CopyStringResult(char *s, int len);
 
 extern void scripting_SetFloat(char *name, float f);
-extern void scripting_DoFile(char *name);
-extern void scripting_DoString(char *command);
-
-
+extern void scripting_RunFile(char *name);
+extern void scripting_Run(char *command);
+extern void scripting_RunFormat(char *format, ...);
+extern void scripting_RunGC();
