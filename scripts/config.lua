02dd1895e16547dec66b8982ed48b3254b5b0785
Andreas Umbach <umbach@gmail.com>
2002-03-22 09:08:11 +0000

    added menu colors

--- scripts/config.lua
+++ scripts/config.lua
@@ -45,6 +45,10 @@ musicVolume = 0.3
 -- this setting is usually overridden in artpack.ini
 clear_color = { 0, 0, 0, 0 }
 
+menu_item = { 1, 1, 1, 1 }
+menu_item_active1 = { .5, 1, 0, 1 }
+menu_item_active2 = { 1, 0, 0, 1 }
+
 model_diffuse_0 = { 1.000, 1.000, 0.000, 1.000 }
 model_diffuse_1 = { 0.700, 0.100, 0.100, 1.000 }
 model_diffuse_2 = { 0.300, 1.000, 0.800, 1.000 }
