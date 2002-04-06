4f1bdec8d7f7a10a8243e15d1e3410128d80d94e
Andreas Umbach <umbach@gmail.com>
2002-04-06 18:10:13 +0000

    - Set/GetFloat/Integer/String -> Set/GetFloat/Integer/StringSetting - all functions use relative stack positions

--- scripting.c
+++ scripting.c
@@ -26,31 +26,36 @@ void scripting_Quit() {
   lua_close(L);
 }
 
-int scripting_GetFloat(char *name, float *f) {
+int scripting_GetFloatSetting(char *name, float *f) {
   int status;
-  lua_getglobal(L, name);
-  if(lua_isnumber(L, 1)) {
+  lua_getglobal(L, "settings");
+  lua_pushstring(L, name);
+  lua_gettable(L, -2);
+
+  if(lua_isnumber(L, -1)) {
     status = 0; /* all's well */
-    *f = lua_tonumber(L, 1);
+    *f = lua_tonumber(L, -1);
   } else
     status = 1;
 
-  lua_pop(L, 1); /* restore stack */
+  lua_pop(L, 2); /* restore stack */
   return status;
 }
 
 
-int scripting_GetInteger(char *name, int *i) {
-  lua_getglobal(L, name);
-  return scripting_GetIntegerResult(i);
+int scripting_GetIntegerSetting(char *name, int *i) {
+  float f;
+  int status;
+  status = scripting_GetFloatSetting(name, &f);
+  *i = f;
+  return status;
 }
 
 int scripting_GetIntegerResult(int *i) {
   int status;
-  int top = lua_gettop(L);
-   if(lua_isnumber(L, top)) {
+   if(lua_isnumber(L, -1)) {
     status = 0; /* all's well */
-    *i = (int)lua_tonumber(L, top);
+    *i = (int)lua_tonumber(L, -1);
   } else
     status = 1;
 
@@ -58,17 +63,20 @@ int scripting_GetIntegerResult(int *i) {
   return status;
 }  
 
-void scripting_SetFloat(char *name, float f) {
+void scripting_SetFloatSetting(char *name, float f) {
+  lua_getglobal(L, "settings");
+  lua_pushstring(L, name);
   lua_pushnumber(L, f);
-  lua_setglobal(L, name);
+  lua_settable(L, -3);
+  lua_pop(L, 1);
 }
 
 void scripting_GetFloatArray(char *name, float *f, int n) {
   int i;
   lua_getglobal(L, name);
   for(i = 0; i < n; i++) {
-    lua_rawgeti(L, 1, i + 1);
-    if(lua_isnumber(L, 2)) {
+    lua_rawgeti(L, -1, i + 1);
+    if(lua_isnumber(L, -1)) {
       *(f + i) = lua_tonumber(L, 2);
     } else {
       fprintf(stderr, "element %d is not number!\n", i);
@@ -78,20 +86,24 @@ void scripting_GetFloatArray(char *name, float *f, int n) {
   lua_pop(L, 1); /* restore stack */
 }
 
-int scripting_GetString(char *name, char **s) {
-  lua_getglobal(L, name);
-  return scripting_GetStringResult(s);
+int scripting_GetStringSetting(char *name, char **s) {
+  int status;
+  lua_getglobal(L, "settings");
+  lua_pushstring(L, name);
+  lua_gettable(L, -2);
+  status = scripting_GetStringResult(s);
+  lua_pop(L, 1);
+  return status;
 }
 
 int scripting_GetStringResult(char **s) {
   int status;
-  int top = lua_gettop(L);
-  if(lua_isstring(L, top)) {
+  if(lua_isstring(L, -1)) {
     int size;
     status = 0;
-    size = lua_strlen(L, top) + 1;
+    size = lua_strlen(L, -1) + 1;
     *s = malloc( size );
-    memcpy( *s, lua_tostring(L, top), size );
+    memcpy( *s, lua_tostring(L, -1), size );
     /* printf("allocated string '%s' of size %d\n", *s, size); */
   } else
     status = 1;
@@ -102,14 +114,13 @@ int scripting_GetStringResult(char **s) {
 
 int scripting_CopyStringResult(char *s, int len) {
   int status;
-  int top = lua_gettop(L);
-  if(lua_isstring(L, top)) {
+  if(lua_isstring(L, -1)) {
     int size, copy;
     status = 0;
-    size = lua_strlen(L, top) + 1;
+    size = lua_strlen(L, -1) + 1;
     if(size > len) { copy = len; status = 2; }
     else copy = size;
-    memcpy( s, lua_tostring(L, top), size );
+    memcpy( s, lua_tostring(L, -1), size );
   } else
     status = 1;
 
