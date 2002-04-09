127ca846776d5b9f24ab395c3bccfd60d1f3a675
tkirby <tkirby@962c35fc-89ae-4f3e-9c41-567b7e30456f>
2002-04-09 07:57:40 +0000

    Nicer default cycle colors.

--- scripts/config.lua
+++ scripts/config.lua
@@ -103,18 +103,18 @@ menu_item = { 1, 1, 1, 1 }
 menu_item_active1 = { .5, 1, 0, 1 }
 menu_item_active2 = { 1, 0, 0, 1 }
 
-model_diffuse_0 = { 1.000, 1.000, 0.000, 1.000 }
-model_diffuse_1 = { 0.700, 0.100, 0.100, 1.000 }
-model_diffuse_2 = { 0.300, 1.000, 0.800, 1.000 }
+model_diffuse_0 = { 1.000, 0.550, 0.140, 1.000 }
+model_diffuse_1 = { 0.750, 0.020, 0.020, 1.000 }
+model_diffuse_2 = { 0.120, 0.520, 0.600, 1.000 }
 model_diffuse_3 = { 0.800, 0.800, 0.800, 1.000 }
 
-model_specular_0 = { 1.000, 1.000, 1.000, 1.000 }
-model_specular_1 = { 1.000, 1.000, 1.000, 1.000 }
-model_specular_2 = { 1.000, 1.000, 1.000, 1.000 }
+model_specular_0 = { 0.500, 0.500, 0.000, 1.000 }
+model_specular_1 = { 0.750, 0.020, 0.020, 1.000 }
+model_specular_2 = { 0.120, 0.520, 0.600, 1.000 }
 model_specular_3 = { 1.000, 1.000, 1.000, 1.000 }
 
-trail_diffuse_0 = { 0.600, 0.600, 0.000, 0.600 }
-trail_diffuse_1 = { 0.500, 0.050, 0.250, 0.600 }
-trail_diffuse_2 = { 0.200, 0.600, 0.400, 0.600 }
+trail_diffuse_0 = { 1.000, 0.850, 0.140, 0.600 }
+trail_diffuse_1 = { 0.750, 0.020, 0.020, 0.600 }
+trail_diffuse_2 = { 0.120, 0.520, 0.600, 0.600 }
 trail_diffuse_3 = { 0.700, 0.700, 0.700, 0.600 }
 
