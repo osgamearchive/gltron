df1527752396ef952fb325ddd5eb695fae646e8f
tkirby <tkirby@962c35fc-89ae-4f3e-9c41-567b7e30456f>
2002-04-17 07:16:31 +0000

    new file.

---
+++ recognizer.h
@@ -0,0 +1,7 @@
+
+/* recognizer stuff */
+extern void initRecognizer();
+extern void recognizerMovement();
+extern void drawRecognizer();
+extern void drawRecognizerShadow();
+extern void getRecognizerPositionVelocity(Point *p, Point *v);
