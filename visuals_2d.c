51a435f33d9682ff04125f2f26daba486d4a55db
Andreas Umbach <umbach@gmail.com>
2002-12-20 16:19:24 +0000

    tweaked 2d map

--- visuals_2d.c
+++ visuals_2d.c
@@ -30,14 +30,33 @@ void draw2D( gDisplay *d ) {
 				foo = 1;
 		}
 
+		glViewport(d->vp_x, d->vp_y, d->vp_w, d->vp_h);
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		glOrtho(0, width, 0, height, 0, 1);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 		glTranslatef(border_left, border_bottom, 0);
+
+		{
+			float w = game2->grid.width;
+			float h = game2->grid.height;
+
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+			glBegin(GL_QUADS);
+			glColor4f(0.7, 0.7, 0.7, 0.3);
+			glVertex2f(0, 0);
+			glVertex2f(w, 0);
+			glVertex2f(w, h);
+			glVertex2f(0, h);
+			glEnd();
+		}
+		
 		
 		glDisable(GL_LIGHTING);
+		// glDisable(GL_BLEND);
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_ONE, GL_ONE);
 		{
@@ -59,7 +78,7 @@ void draw2D( gDisplay *d ) {
 				if(p->data->speed < 0)
 						continue;
 				
-				glColor3fv( p->pColorDiffuse );
+				glColor3fv( p->pColorAlpha );
 				glPointSize(2);
 				glBegin(GL_POINTS);
 				glVertex2f(p->data->iposx, p->data->iposy);
@@ -85,6 +104,5 @@ void draw2D( gDisplay *d ) {
 				glEnd();
 		}
 		glDisable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 		
