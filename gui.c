5634f5c324cd7b80bfbc2e76abb890c0bfca8939
Andreas Umbach <umbach@gmail.com>
2002-12-20 16:15:04 +0000

    added joystick buttons/axis

--- gui.c
+++ gui.c
@@ -161,18 +161,46 @@ void keyboardGui(int key, int x, int y) {
     break;
   case ' ': 
   case SYSTEM_KEY_RETURN:
+	case SYSTEM_JOY_BUTTON_0:
+	case SYSTEM_JOY_BUTTON_1:
+	case SYSTEM_JOY_BUTTON_2:
+	case SYSTEM_JOY_BUTTON_3:
+	case SYSTEM_JOY_BUTTON_4:
+	case SYSTEM_JOY_BUTTON_5:
+	case SYSTEM_JOY_BUTTON_6:
+	case SYSTEM_JOY_BUTTON_7:
+	case SYSTEM_JOY_BUTTON_8:
+	case SYSTEM_JOY_BUTTON_9:
+	case SYSTEM_JOY_BUTTON_0 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_1 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_2 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_3 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_4 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_5 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_6 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_7 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_8 + SYSTEM_JOY_OFFSET:
+	case SYSTEM_JOY_BUTTON_9 + SYSTEM_JOY_OFFSET:
     scripting_Run("Menu.Action()");
     break;
   case SYSTEM_KEY_UP:
+	case SYSTEM_JOY_UP:
+	case SYSTEM_JOY_UP + SYSTEM_JOY_OFFSET:
     scripting_Run("Menu.Previous()");
     break;
   case SYSTEM_KEY_DOWN:
+	case SYSTEM_JOY_DOWN:
+	case SYSTEM_JOY_DOWN + SYSTEM_JOY_OFFSET:
     scripting_Run("Menu.Next()");
     break;
   case SYSTEM_KEY_RIGHT:
+	case SYSTEM_JOY_RIGHT:
+		case SYSTEM_JOY_RIGHT + SYSTEM_JOY_OFFSET:
     scripting_Run("Menu.Right()");
     break;
   case SYSTEM_KEY_LEFT:
+	case SYSTEM_JOY_LEFT:
+		case SYSTEM_JOY_LEFT + SYSTEM_JOY_OFFSET:
     scripting_Run("Menu.Left()");
   case SYSTEM_KEY_F11: doBmpScreenShot(game->screen); break;
   case SYSTEM_KEY_F12: doPngScreenShot(game->screen); break;
