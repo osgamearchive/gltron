85d933c900ad1db55b352a23e7a2bdfba3bc0e88
Andreas Umbach <umbach@gmail.com>
2003-03-16 16:39:39 +0000

    added main loop

--- scripts/main.lua
+++ scripts/main.lua
@@ -11,9 +11,54 @@ RETURN_TIMEDEMO = 7
 RETURN_TIMEDEMO_ABORT = 8
 RETURN_CREDITS = 9
 RETURN_QUIT = 10
+RETURN_GUI_ESCAPE = 11
+RETURN_PROMPT_ESCAPE = 12
+RETURN_PAUSE_ESCAPE = 13
 
 -- SetCallback("gui")
 -- SystemMainLoop()
 
-SetCallback("timedemo")
-SystemMainLoop()
+callback = "gui"
+last_callback = "gui"
+
+next_callback = {}
+next_callback[ RETURN_GAME_LAUNCH ] = "pause"
+next_callback[ RETURN_GAME_END ] = "pause"
+next_callback[ RETURN_GAME_PAUSE ] = "pause"
+next_callback[ RETURN_GAME_UNPAUSE ] = "game"
+next_callback[ RETURN_CREDITS ] = "credits"
+next_callback[ RETURN_GAME_CREDITS ] = "credits"
+
+next_callback[ RETURN_GAME_ESCAPE ] = "gui"
+next_callback[ RETURN_GUI_ESCAPE ] = "last"
+next_callback[ RETURN_PROMPT_ESCAPE ] = "last"
+next_callback[ RETURN_PAUSE_ESCAPE ] = "gui"
+
+next_callback[ RETURN_GUI_PROMPT ] = "configure"
+next_callback[ RETURN_TIMEDEMO ] = nil
+next_callback[ RETURN_TIMEDEMO_ABORT ] = nil
+next_callback[ RETURN_QUIT ] = nil
+
+
+while 1 do
+	 SetCallback(callback)
+	 write(format("[lua] setting callback '%s'\n", callback))
+	 
+	 status = SystemMainLoop()
+	 write(format("[lua] system returned (%d)\n", status))
+	 
+	 if(next_callback[ status ]) then
+			if(next_callback[ status ] == "last") then
+				 last_callback, callback = callback, last_callback
+			else
+				 last_callback = callback
+				 callback = next_callback[ status ]
+			end
+	 else
+			if(status == 10) then
+				 write(format("[lua] unhandled callback (%d)\n", status))
+				 exit()
+			end
+	 end
+end
+			
