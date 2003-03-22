9b10183b1bb216bf254c8eecd0dced5906d8a1a6
Andreas Umbach <umbach@gmail.com>
2003-03-22 18:09:25 +0000

    moved some functions to graphics_*

--- gamegraphics.c
+++ gamegraphics.c
@@ -4,7 +4,7 @@
 #include "floor.h"
 #include "explosion.h"
 
-static float arena[] = { 1.0, 1.2, 1, 0.0 };
+// static float arena[] = { 1.0, 1.2, 1, 0.0 };
 
 #define MAX_LOD_LEVEL 3
 static int lod_dist[MAX_LOD_LEVEL + 1][LC_LOD + 1] = { 
@@ -62,155 +62,9 @@ void drawGame() {
 
 	if(game2->settingsCache.show_console)
 		drawConsole(game->screen);
-  /*
-    if(getSettingi("show_help") == 1)
-    drawHelp(game->screen);
-  */
 
   /* printf("%d polys\n", polycount); */
 }
-
-void checkGLError(char *where) {
-  int error;
-  error = glGetError();
-  if(error != GL_NO_ERROR)
-    printf("[glError: %s] - %d\n", where, error);
-}
-
-void rasonly(gDisplay *d) {
-  /* do rasterising only (in local display d) */
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(0.0f, (GLfloat) d->vp_w, 0.0f, (GLfloat) d->vp_h, 0.0f, 1.0f);
-  checkGLError("rasonly");
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  glViewport(d->vp_x, d->vp_y, d->vp_w, d->vp_h);
-}
-
-void drawText(fonttex* ftx, int x, int y, int size, char *text) {
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable(GL_TEXTURE_2D);
-
-  glPushMatrix();
-
-  glTranslatef(x, y, 0);
-  glScalef(size, size, size);
-  ftxRenderString(ftx, text, strlen(text));
-  
-  glPopMatrix();
-  glDisable(GL_TEXTURE_2D);
-  glDisable(GL_BLEND);
-  polycount += 2 * strlen(text); /* quads are two triangles */
-}
-
-void initModelLights(int light) {
-  /* float col[] = { .77, .77, .77, 1.0 }; */
-  float col[] = { .95, .95, .95, 1.0 };
-  float dif[] =  { 0.4, 0.4, 0.4, 1};
-  float amb[] = { 0.05, 0.05, 0.05, 1};
-
-  glLightfv(light, GL_AMBIENT, amb);
-  glLightfv(light, GL_SPECULAR, col);
-  glLightfv(light, GL_DIFFUSE, dif);
-  
-}
- 
-void initTrailLights(int light) {
-  /* doing my own lighting for that now */
-  /* int light is currently ignored */
-  float amb[] = { 0.2, 0.2, 0.2, 1};
-  setFactor3fv(amb);
-  setLight4fv(arena);
-}
-
-void drawConsoleLines(char *line, int call) {
-#define CONSOLE_SIZE 15
-#define CONSOLE_X_OFF 20
-  int size = CONSOLE_SIZE;
-  int length;
-  /* fprintf(stdout, "%s\n", line); */
-  length = strlen(line);
-  while(length * size > game->screen->vp_w / 2 - CONSOLE_X_OFF)
-    size--;
-    
-  if(*line != 0) 
-    drawText(gameFtx, CONSOLE_X_OFF, game->screen->vp_h - 20 * (call + 1),
-	     size, line);
-}
-
-void drawConsole(gDisplay *d) {
-  int lines;
-  rasonly(d);
-  glColor3f(1.0, 0.3, 0.3);
-  
-  if (game2->settingsCache.softwareRendering) { 
-    lines = 1;
-  } else if (game->screen->vp_h < 600) {
-    lines = 3;
-  } else {
-    lines = 5;
-  }
-  
-  consoleDisplay(drawConsoleLines, lines);
-}
-  
-void drawFPS(gDisplay *d) {
-#define FPS_HSIZE 20
-  /* draws FPS in upper left corner of Display d */
-  static int fps_h[FPS_HSIZE];
-  static int pos = -FPS_HSIZE;
-  static int fps_min = 0;
-  static int fps_avg = 0;
-
-  char tmp[20];
-  int diff;
-
-  rasonly(d);
-  diff = (game2->time.dt > 0) ? game2->time.dt : 1;
-
-  if(pos < 0) {
-    fps_avg = 1000 / diff;
-    fps_min = 1000 / diff;
-    fps_h[pos + FPS_HSIZE] = 1000 / diff;
-    pos++;
-  } else {
-    fps_h[pos] = 1000 / diff;
-    pos = (pos + 1) % FPS_HSIZE;
-    if(pos % 10 == 0) {
-      int i;
-      int sum = 0;
-      int min = 1000;
-      for(i = 0; i < FPS_HSIZE; i++) {
-	sum += fps_h[i];
-	if(fps_h[i] < min)
-	  min = fps_h[i];
-      }
-      fps_min = min;
-      fps_avg = sum / FPS_HSIZE;
-    }
-  }
-
-  sprintf(tmp, "average FPS: %d", fps_avg);
-  glColor4f(1.0, 0.4, 0.2, 1.0);
-  drawText(gameFtx, d->vp_w - 180, d->vp_h - 20, 10, tmp);
-  sprintf(tmp, "minimum FPS: %d", fps_min);
-  drawText(gameFtx, d->vp_w - 180, d->vp_h - 35, 10, tmp);
-  sprintf(tmp, "triangles: %d", polycount);
-  drawText(gameFtx, d->vp_w - 180, d->vp_h - 50, 10, tmp);
-}
-
-void drawScore(Player *p, gDisplay *d) {
-  char tmp[10]; /* hey, they won't reach such a score */
-
-  sprintf(tmp, "%d", p->data->score);
-  rasonly(d);
-  glColor4f(1.0, 1.0, 0.2, 1.0);
-  drawText(gameFtx, 5, 5, 32, tmp);
-}
-
-/* distance from point v to line p + t * d */
  
 float GetDistance(float *v, float *p, float *d) {
   float diff[3];
@@ -327,27 +181,6 @@ void drawCycleShadow(Player *p, int lod, int drawTurn) {
   glPopMatrix();
 }
 
-static void drawImpact(Player *p) {
-
-  glDisable(GL_LIGHTING);
-  glPushMatrix();
-
-  glRotatef(90, 90, 0, 1);
-
-  /*
-   * Drop impact slightly below the floor and move it a tiny bit
-   * back from the impact point so we don't draw behind the
-   * trail/wall that's hit.
-   */
-  glTranslatef(0.0, -0.5, -0.5);
-
-  glColor3f(0.68, 0, 0);
-  
-  drawExplosion(&(p->data->impact_radius));
-  glPopMatrix();
-  glEnable(GL_LIGHTING);
-}
-
 void drawCycle(Player *p, int lod, int drawTurn) {
   Mesh *cycle;
   unsigned int  time, turn_time;
@@ -370,10 +203,8 @@ void drawCycle(Player *p, int lod, int drawTurn) {
     doCycleTurnRotation(p);
   }
 
-  initModelLights(GL_LIGHT1);
-  glDisable(GL_LIGHT0);
-  glEnable(GL_LIGHT1);
-
+	setupLights(eCycles);
+	
   SetMaterialColor(cycle, "Hull", eDiffuse, p->pColorDiffuse); 
   SetMaterialColor(cycle, "Hull", eSpecular, p->pColorSpecular); 
 
@@ -486,174 +317,6 @@ void drawPlayers(Player *p) {
   }
 }
 
-void drawGlow(Player *p, gDisplay *d, float dim) {
-  float mat[4*4];
-  
-  glPushMatrix();
-  glTranslatef(p->data->posx,
-	       p->data->posy,
-	       0);
-
-  glShadeModel(GL_SMOOTH);
-
-  glDepthMask(GL_FALSE);
-  glEnable(GL_DEPTH_TEST);
-
-  glBlendFunc(GL_ONE, GL_ONE);
-  glEnable(GL_BLEND);
-
-  glGetFloatv(GL_MODELVIEW_MATRIX, mat);
-  mat[0] = mat[5] = mat[10] = 1.0;
-  mat[1] = mat[2] = 0.0;
-  mat[4] = mat[6] = 0.0;
-  mat[8] = mat[9] = 0.0;
-  glLoadMatrixf(mat);
-  glBegin(GL_TRIANGLE_FAN);
-  glColor3fv(p->pColorDiffuse);
-
-  glVertex3f(0,TRAIL_HEIGHT/2, 0);
-  glColor4f(0,0,0,0.0);
-  glVertex3f(dim*cos(-0.2*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(-0.2*3.1415/5.0), 0);
-  glVertex3f(dim*cos(1.0*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(1.0*3.1415/5.0), 0);
-  glVertex3f(dim*cos(2.0*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(2.0*3.1415/5.0), 0);
-  glVertex3f(dim*cos(3.0*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(3.0*3.1415/5.0), 0);
-  glVertex3f(dim*cos(4.0*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(4.0*3.1415/5.0), 0);
-  glVertex3f(dim*cos(5.2*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(5.2*3.1415/5.0), 0);
-  glEnd();
-  polycount += 5;
-
-
-  glBegin(GL_TRIANGLES);
-  glColor3fv(p->pColorDiffuse);
-  glVertex3f(0,TRAIL_HEIGHT/2, 0);
-  glColor4f(0,0,0,0.0);
-  glVertex3f(0,-TRAIL_HEIGHT/4,0);
-  glVertex3f(dim*cos(-0.2*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(-0.2*3.1415/5.0), 0);
-
-  glColor3fv(p->pColorDiffuse);
-  glVertex3f(0,TRAIL_HEIGHT/2, 0);
-  glColor4f(0,0,0,0.0);
-  glVertex3f(dim*cos(5.2*3.1415/5.0),
-	     TRAIL_HEIGHT/2+dim*sin(5.2*3.1415/5.0), 0);
-  glVertex3f(0,-TRAIL_HEIGHT/4,0);
-  glEnd();
-  polycount += 3;
-
-  glDepthMask(GL_TRUE);
-  glEnable(GL_DEPTH_TEST);
-
-  glShadeModel( game->screen->shademodel );
-  glPopMatrix();  
-
-
-}
-
-void drawWalls() {
-#undef WALL_H
-#define WALL_H 48
-  float t;
-  float h;
-
-  t = game2->rules.grid_size / 240.0;
-  if (game2->settingsCache.stretch_textures) {
-    h = t * WALL_H;
-    t = 1.0;
-  } else h = WALL_H;
-
-  glColor4f(1.0, 1.0, 1.0, 1.0);
-
-
-  /*
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  */
-  glEnable(GL_CULL_FACE);
-  glEnable(GL_TEXTURE_2D);
-  glEnable(GL_BLEND);
-#define T_TOP 1.0
-  glBindTexture(GL_TEXTURE_2D, game->screen->textures[TEX_WALL1]);
-  glBegin(GL_QUADS);
-  glTexCoord2f(t, 0.0); glVertex3f(0.0, 0.0, 0.0);
-  glTexCoord2f(t, T_TOP); glVertex3f(0.0, 0.0, h);
-  glTexCoord2f(0.0, T_TOP); glVertex3f(game2->rules.grid_size, 0.0, h);
-  glTexCoord2f(0.0, 0.0); glVertex3f(game2->rules.grid_size, 0.0, 0.0);
-  glEnd();
-  
-  glBindTexture(GL_TEXTURE_2D, game->screen->textures[TEX_WALL2]);
-  glBegin(GL_QUADS);
-  glTexCoord2f(t, 0.0); glVertex3f(game2->rules.grid_size, 0.0, 0.0);
-  glTexCoord2f(t, T_TOP); glVertex3f(game2->rules.grid_size, 0.0, h);
-  glTexCoord2f(0.0, T_TOP); 
-  glVertex3f(game2->rules.grid_size, game2->rules.grid_size, h);
-  glTexCoord2f(0.0, 0.0); 
-  glVertex3f(game2->rules.grid_size, game2->rules.grid_size, 0.0);
-  glEnd();
-  
-  glBindTexture(GL_TEXTURE_2D, game->screen->textures[TEX_WALL3]);
-  glBegin (GL_QUADS);
-  glTexCoord2f(t, 0.0); 
-  glVertex3f(game2->rules.grid_size, game2->rules.grid_size, 0.0);
-  glTexCoord2f(t, T_TOP); 
-  glVertex3f(game2->rules.grid_size, game2->rules.grid_size, h);
-  glTexCoord2f(0.0, T_TOP); glVertex3f(0.0, game2->rules.grid_size, h);
-  glTexCoord2f(0.0, 0.0); glVertex3f(0.0, game2->rules.grid_size, 0.0);
-  glEnd();
-  
-  glBindTexture(GL_TEXTURE_2D, game->screen->textures[TEX_WALL4]);
-  glBegin(GL_QUADS);
-  glTexCoord2f(t, 0.0); glVertex3f(0.0, game2->rules.grid_size, 0.0);
-  glTexCoord2f(t, T_TOP); glVertex3f(0.0, game2->rules.grid_size, h);
-  glTexCoord2f(0.0, T_TOP); glVertex3f(0.0, 0.0, h);
-  glTexCoord2f(0.0, 0.0); glVertex3f(0.0, 0.0, 0.0); 
-#undef T_TOP
-  glEnd();
-  polycount += 8;
-
-  glDisable(GL_BLEND);
-  glDisable(GL_TEXTURE_2D);
-  glDisable(GL_CULL_FACE);
-}
-
-
-void doPerspective(float fov, float ratio, float znear, float zfar) {
-  float top;
-  float left;
-
-  top = tan( fov * M_PI / 360.0 ) * znear;
-  left = - top * ratio;
-  glFrustum(left, -left, -top, top, znear, zfar);
-}
-
-void doLookAt(float *cam, float *target, float *up) {
-  float m[16];
-  float x[3], y[3], z[3];
-
-  vsub(cam, target, z);
-  normalize(z);
-  crossprod(up, z, x);
-  crossprod(z, x, y);
-  normalize(x);
-  normalize(y);
-
-#define M(row,col)  m[col*4+row]
-  M(0,0) = x[0];  M(0,1) = x[1];  M(0,2) = x[2];  M(0,3) = 0.0;
-  M(1,0) = y[0];  M(1,1) = y[1];  M(1,2) = y[2];  M(1,3) = 0.0;
-  M(2,0) = z[0];  M(2,1) = z[1];  M(2,2) = z[2];  M(2,3) = 0.0;
-  M(3,0) = 0.0;   M(3,1) = 0.0;   M(3,2) = 0.0;   M(3,3) = 1.0;
-#undef M
-  glMultMatrixf( m );
-
-  /* Translate Eye to Origin */
-  glTranslatef( -cam[0], -cam[1], -cam[2]);
-}
-
 void drawCam(Player *p, gDisplay *d) {
   int i;
   float up[3] = { 0, 0, 1 };
@@ -733,14 +396,14 @@ void drawCam(Player *p, gDisplay *d) {
   }
 
   if (game2->settingsCache.show_wall == 1) {
-    drawWalls(d);
+    drawWalls();
   }
 
   drawPlayers(p);
 
   glShadeModel(game->screen->shademodel);
 
-  initTrailLights(0);
+	setupLights(eWorld);
 
   glEnable(GL_POLYGON_OFFSET_FILL);
   glPolygonOffset(1,1);
@@ -785,87 +448,6 @@ void drawCam(Player *p, gDisplay *d) {
 		glDepthMask(GL_TRUE);
 		glEnable(GL_DEPTH_TEST);
 	}
-	
-  /*if(getSettingi("show_recognizer")) {
-#if DEPTH_CLEAR
-    glClear(GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
-#endif
-    glDepthMask(GL_TRUE);
-
-#if DEPTH_CLEAR
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    doPerspective(getSettingi("fov"), d->vp_w / d->vp_h,
-		  getSettingi("znear") * 2, game2->rules.grid_size * 6.5);
-
-    glMatrixMode(GL_MODELVIEW);
-#endif
-
-    glEnable(GL_CULL_FACE);
-    drawRecognizers(1);
-    glDisable(GL_CULL_FACE);
-  }*/
-}
-
-void drawAI(gDisplay *d) {
-  char ai[] = "computer player";
-
-  rasonly(d);
-  glColor3f(1.0, 1.0, 1.0);
-  drawText(gameFtx, d->vp_w / 4, 10, d->vp_w / (2 * strlen(ai)), ai);
-  /* glRasterPos2i(100, 0); */
-}
-
-void drawPause(gDisplay *display) {
-  char pause[] = "Game is paused";
-  char winner[] = "Player %d wins!";
-  char nowinner[] = "No one wins!";
-  char buf[100];
-  char *message;
-  static float d = 0;
-  static float lt = 0;
-  float delta;
-  long now;
-
-  now = SystemGetElapsedTime();
-  delta = now - lt;
-  lt = now;
-  delta /= 500.0;
-  d += delta;
-  /* printf("%.5f\n", delta); */
-  
-  if (d > 2 * M_PI) { 
-    d -= 2 * M_PI;
-  }
-
-  if ((game->pauseflag & PAUSE_GAME_FINISHED) && game->winner != -1) {
-    if (game->winner >= -1) {
-
-      float* player_color = game->player[game->winner].pColorAlpha;
-
-      /* 
-         make the 'Player wins' message oscillate between 
-         white and the winning bike's color 
-       */
-      glColor3f((player_color[0] + ((sin(d) + 1) / 2) * (1 - player_color[0])),
-                (player_color[1] + ((sin(d) + 1) / 2) * (1 - player_color[1])),
-                (player_color[2] + ((sin(d) + 1) / 2) * (1 - player_color[2]))); 
-
-      message = buf;
-      sprintf(message, winner, game->winner + 1);
-    } else {
-      glColor3f(1.0, (sin(d) + 1) / 2, (sin(d) + 1) / 2);
-      message = nowinner;
-    }
-  } else {
-    glColor3f(1.0, (sin(d) + 1) / 2, (sin(d) + 1) / 2);
-    message = pause;
-  }
-
-  rasonly(game->screen);
-  drawText(gameFtx, display->vp_w / 6, 20, 
-	   display->vp_w / (6.0 / 4.0 * strlen(message)), message);
 }
 
 void initGLGame() {
