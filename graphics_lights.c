92fafe7d2bdb50ef33e7c531120dc4ca506f841e
Andreas Umbach <umbach@gmail.com>
2003-03-31 18:42:33 +0000

    tweaked world lights a little

--- graphics_lights.c
+++ graphics_lights.c
@@ -2,13 +2,13 @@
 
 void setupLights(int type) {
 	float white[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	float gray22[] = { 0.22f, 0.22f, 0.22f, 1.0f };
+	// float gray22[] = { 0.22f, 0.22f, 0.22f, 1.0f };
 	float gray66[] = { 0.66f, 0.66f, 0.66f, 1.0f };
 	float gray10[] = { 0.1f, 0.1f, 0.1f, 1.0f };
 	float black[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 	
-	float posWorld0[] = { 1, .2, 0, 0 };
-	float posWorld1[] = { -1, -.2, 0, 0 };
+	float posWorld0[] = { 1, .8, 0, 0 };
+	float posWorld1[] = { -1, -.8, 0, 0 };
 
 	float posCycles0[] = { 0, 0, 0, 1 };
 	
@@ -31,17 +31,14 @@ void setupLights(int type) {
 		glLightfv(GL_LIGHT0, GL_POSITION, posWorld0);
 		glLightfv(GL_LIGHT0, GL_AMBIENT, gray10);
 		// glLightfv(GL_LIGHT0, GL_AMBIENT, black);
-		glLightfv(GL_LIGHT0, GL_SPECULAR, gray66);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, white);
 		glLightfv(GL_LIGHT0, GL_DIFFUSE, white);
 
 		glEnable(GL_LIGHT1);
 		glLightfv(GL_LIGHT1, GL_POSITION, posWorld1);
 		glLightfv(GL_LIGHT1, GL_AMBIENT, black);
-		// glLightfv(GL_LIGHT1, GL_AMBIENT, black);
-		glLightfv(GL_LIGHT1, GL_SPECULAR, gray22);
-		glLightfv(GL_LIGHT1, GL_DIFFUSE, gray22);
-		
-		// glDisable(GL_LIGHT1);
+		glLightfv(GL_LIGHT1, GL_SPECULAR, gray66);
+		glLightfv(GL_LIGHT1, GL_DIFFUSE, gray66);
 		
 		for(i = 2; i < 8; i++)
 				glDisable(GL_LIGHT0 + i);
