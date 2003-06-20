8edd684f74d747c32bfff5787a0a67d624e76fe8
Andreas Umbach <umbach@gmail.com>
2003-06-20 14:54:42 +0000

    directory reorganization

---
+++ src-base/include/audio/nebu_SourceMusic.h
@@ -0,0 +1,42 @@
+#ifndef NEBU_Sound_SourceMusic_H
+#define NEBU_Sound_SourceMusic_H
+
+#include "nebu_Sound.h"
+
+#include "nebu_Source.h"
+#include "nebu_SoundSystem.h"
+
+namespace Sound {
+  class SourceMusic : public Source {
+  public:
+    SourceMusic(System *system);
+    virtual ~SourceMusic();
+    void Load(char *filename);
+    virtual int Mix(Uint8 *data, int len);
+		virtual void Idle(void);
+
+  protected:
+    virtual void Reset(void) { 
+      if(_sample != NULL) {
+				CleanUp();
+				CreateSample();
+				// fprintf(stderr, "sample resetted\n");
+      };
+    };
+    void CleanUp(void);
+    void CreateSample(void);
+
+  private:
+    Sound_Sample* _sample;
+		Uint32 _sample_buffersize;
+		
+		Uint8* _buffer;
+    Uint32 _buffersize;
+    Uint32 _read;
+    Uint32 _decoded;
+
+		char *_filename;
+    SDL_RWops *_rwops;
+  };
+}
+#endif
