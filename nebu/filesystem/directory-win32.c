9d4ae1d8d94f5e539da66fd7c90df2e13ca3a056
ahowe <ahowe@962c35fc-89ae-4f3e-9c41-567b7e30456f>
2003-07-01 06:01:44 +0000

    Directory functions should resemble something that actually works now.

--- nebu/filesystem/directory-win32.c
+++ nebu/filesystem/directory-win32.c
@@ -15,27 +15,63 @@
 /* FIXME: This is really broken. */
 List* readDirectoryContents(const char *dirname, const char *prefix) {
 	WIN32_FIND_DATA search;
-	HANDLE hDir;
-	List *currentList;
-	char *file;
+	HANDLE hSearch;
+	List *l, *f;
+	char *filename, *searchStr;
+	int   len, prefixLen = 0;
 
-	currentList = malloc(sizeof(List));
-	currentList->next = malloc(sizeof(List));
-	currentList->next->next = NULL;
+	l = malloc(sizeof(List));
+	l->next= NULL;
+	f = l;
 
-	hDir = FindFirstFile(dirname, &search);
-	if(hDir == 0)
+	if(prefix != NULL)
 	{
+		prefixLen = strlen(prefix);
+	}
+	searchStr = malloc(strlen(dirname) + prefixLen + 3);
+	memcpy(searchStr, dirname, strlen(dirname) + 1);
+	strcat(searchStr, "\\");
+	if(prefix != NULL)
+	{
+		strcat(searchStr, prefix);
+	}
+	strcat(searchStr, "*");
+
+
+	hSearch = FindFirstFile(searchStr, &search);
+	if(hSearch == INVALID_HANDLE_VALUE)
+	{
+		free(l);
+		free(searchStr);
 		return NULL;
 	}
-	file = malloc(strlen(search.cFileName) + 1);
-	strcpy(file,search.cFileName);
+
+	len = strlen(search.cFileName) + 1;
+	filename = malloc(len);
+	memcpy(filename, search.cFileName, len);
+	l->data = filename;
+
+	while( FindNextFile(hSearch, &search) )
+	{
+		l->next = malloc(sizeof(List));
+		l = l->next;
+		len = strlen(search.cFileName) + 1;
+		filename = malloc(len);
+		memcpy(filename, search.cFileName, len);
+		l->data = filename;
+		l->next = NULL;
+	}
+
+	if(GetLastError() != ERROR_NO_MORE_FILES)
+	{
+		fprintf(stderr, "[directory] Error: something bad happened\n");
+	}
 	
-	currentList->next->data = file;
-	currentList->data = file;
 
+	FindClose(hSearch);
+	free(searchStr);
 
-	return currentList;
+	return f;
 
 }
 
