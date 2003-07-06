c0e198ad4c521b05d942d031ae0a57b4af178dbc
Andreas Umbach <umbach@gmail.com>
2003-07-06 20:28:25 +0000

    changed defaults

--- scripts/config.lua
+++ scripts/config.lua
@@ -71,12 +71,12 @@ settings.joy_threshold = 0.10
 
 -- audio
 settings.tracks = { }
-settings.current_track = "song_revenge_of_cats.it"
+settings.current_track = "revenge_of_cats.it"
 -- music & fx settings
 settings.playEffects = 1
 settings.playMusic = 1
-settings.fxVolume = 0.3
-settings.musicVolume = 0.3
+settings.fxVolume = 0.2
+settings.musicVolume = 0.5
 
 -- keys
 
