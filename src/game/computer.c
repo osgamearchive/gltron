20e5b4797ef8946f41aacc8e696e2c326649c46c
Andreas Umbach <umbach@gmail.com>
2003-09-27 19:21:47 +0000

    AI code updated for new collision detection

--- src/game/computer.c
+++ src/game/computer.c
@@ -2,54 +2,141 @@
 #include "video/video.h"
 
 #include <math.h>
+#include <float.h>
 
-static void getDistPoint(int dir, int d, int *x, int *y) {
-  *x += dirsX[dir] * d;
-  *y += dirsY[dir] * d;
-}
-
-static void setPos(const Data *data, int *x, int *y) {
-  *x = (int)data->posx;
-  *y = (int)data->posy;
-}
-
-#define MAX_PROBE_DIST 20
+#ifndef FLT_MAX
+#define FLT_MAX 10000.0f
+#endif
 
 #define LEFT(x) ((x + 3) % 4)
 #define RIGHT(x) ((x + 1) % 4)
 
-static int probe_dists[][2] = {
-  { 8, 10 },
-  { 2, 5 },
+// time to pass between turns, in milliseconds
+static int minTurnTime[] = {
+  600, 400, 200, 100
 };
 
-/*
-	static int target_dir[][2] = {
-  { 0, 1 },
-  { 2, 1 },
-  { 3, 0 },
-  { 3, 2 }
-	};
-*/
-
-static int turn_time[] = {
-  200, 100
+static float maxSegLength[] = {
+	0.6, 0.3, 0.3, 0.3
 };
 
-static float max_moves[] = {
-  0.4f, 0.5f
+static float critical[] = {
+	0.2, 0.08037, 0.08037, 0.08037
 };
 
+// turns until a spiral is detected
 static int spiral[] = {
-  10, 10
+  10, 10, 10, 10
 };
+
+static int rlDelta[] = {
+	0, 10, 20, 30
+};
+
+void getDistances(int player, 
+									float* front, float *left, float *right, float *backleft) {
+	enum { eFront = 0, eLeft, eRight, eBackleft, eMax };
+	segment2 segments[eMax];
+	vec2 v, vPos;
+	Data *data = game->player[player].data;
+	int dirLeft = (data->dir + 3) % 4;
+	int dirRight = (data->dir + 1) % 4;
+	int i, j;
 	
+	vPos.v[0] = data->posx;
+	vPos.v[1] = data->posy;
+
+	for(i = 0; i < eMax; i++) {
+		vec2Copy(&segments[i].vStart, &vPos);
+	}
+
+	segments[eFront].vDirection.v[0] = dirsX[data->dir];
+	segments[eFront].vDirection.v[1] = dirsY[data->dir];
+	segments[eLeft].vDirection.v[0] = dirsX[dirLeft];
+	segments[eLeft].vDirection.v[1] = dirsY[dirLeft];
+	segments[eRight].vDirection.v[0] = dirsX[dirRight];
+	segments[eRight].vDirection.v[1] = dirsY[dirRight];
+	segments[eBackleft].vDirection.v[0] = dirsX[dirLeft] - dirsX[data->dir];
+	segments[eBackleft].vDirection.v[1] = dirsY[dirLeft] - dirsY[data->dir];
+	vec2Normalize(&segments[eBackleft].vDirection,
+								&segments[eBackleft].vDirection);
+	*front = FLT_MAX;
+	*left = FLT_MAX;
+	*right = FLT_MAX;
+	*backleft = FLT_MAX;
+
+	// loop over all segment
+	for(i = 0; i < game->players; i++) {
+		segment2 *wall = game->player[i].data->trails;
+		for(j = 0; j < game->player[i].data->trailOffset + 1; j++) {
+			float t1, t2;
+			if(i == player && j == game->player[i].data->trailOffset)
+				break;
+			if(segment2_Intersect(&v, &t1, &t2, segments + eFront, wall) &&
+				 t1 > 0 && t1 < *front && t2 >= 0 && t2 <= 1)
+				*front = t1;
+			if(segment2_Intersect(&v, &t1, &t2, segments + eLeft, wall) &&
+				 t1 > 0 && t1 < *left && t2 >= 0 && t2 <= 1)
+				*left = t1;
+			if(segment2_Intersect(&v, &t1, &t2, segments + eRight, wall) &&
+				 t1 > 0 && t1 < *right && t2 >= 0 && t2 <= 1)
+				*right = t1;
+			if(segment2_Intersect(&v, &t1, &t2, segments + eBackleft, wall) &&
+				 t1 > 0 && t1 < *backleft && t2 >= 0 && t2 <= 1)
+				*backleft = t1;
+			wall++;
+		}
+	}
+	for(i = 0; i < 4; i++) {
+		float t1, t2;
+		segment2* wall = walls + i;
+		if(segment2_Intersect(&v, &t1, &t2, segments + eFront, wall) &&
+			 t1 > 0 && t1 < *front && t2 >= 0 && t2 <= 1)
+			*front = t1;
+		if(segment2_Intersect(&v, &t1, &t2, segments + eLeft, wall) &&
+			 t1 > 0 && t1 < *left && t2 >= 0 && t2 <= 1)
+			*left = t1;
+		if(segment2_Intersect(&v, &t1, &t2, segments + eRight, wall) &&
+			 t1 > 0 && t1 < *right && t2 >= 0 && t2 <= 1)
+			*right = t1;
+		if(segment2_Intersect(&v, &t1, &t2, segments + eBackleft, wall) &&
+			 t1 > 0 && t1 < *backleft && t2 >= 0 && t2 <= 1)
+			*backleft = t1;
+	}
+	
+	// update debug render segments
+	{
+		AI *ai = game->player[player].ai;
+		vec2Copy(&ai->front.vStart, &vPos);
+		vec2Copy(&ai->left.vStart, &vPos);
+		vec2Copy(&ai->right.vStart, &vPos);
+		vec2Copy(&ai->backleft.vStart, &vPos);
+		
+		ai->front.vDirection.v[0] = *front * dirsX[data->dir];
+		ai->front.vDirection.v[1] = *front * dirsY[data->dir];
+		ai->left.vDirection.v[0] = *left * dirsX[dirLeft];
+		ai->left.vDirection.v[1] = *left * dirsY[dirLeft];
+		ai->right.vDirection.v[0] = *right * dirsX[dirRight];
+		ai->right.vDirection.v[1] = *right * dirsY[dirRight];
+		ai->backleft.vDirection.v[0] = dirsX[dirLeft] - dirsX[data->dir];
+		ai->backleft.vDirection.v[1] = dirsY[dirLeft] - dirsY[data->dir];
+		vec2Normalize(&ai->backleft.vDirection,
+									&ai->backleft.vDirection);
+		vec2Scale(&ai->backleft.vDirection, 
+							&ai->backleft.vDirection,
+							*backleft);
+	}
+		
+	// printf("%.2f, %.2f, %.2f\n", *front, *right, *left);
+	return;
+}
+
 void doComputer(int player, int target) {
   AI *ai;
   Data *data;
   Player *me, *him;
-  int i, j, level, x, y;
-  int rdist = 0, ldist = 0;
+  int level;
+	float front, left, right, backleft;
 
   me = &(game->player[ player ]);
   him = &(game->player[ target ]);
@@ -61,182 +148,49 @@ void doComputer(int player, int target) {
   ai = me->ai;
   level = game2->settingsCache.ai_level;
 
-  ai->moves++;
   /* avoid too short turns */
-  if(game2->time.current - ai->lasttime < turn_time[level])
+  if(game2->time.current - ai->lasttime < minTurnTime[level])
     return;
 
+	getDistances(player, &front, &left, &right, &backleft);
 
-  /* first, check if we are in danger */
-  /* check the highest level first! */
-  for(i = level; i >= 0; i++) {
-    for(j = probe_dists[i][0]; j <= probe_dists[i][1]; j++) {
-      setPos(data, &x, &y);
-      getDistPoint(data->dir, j, &x, &y);
-      if(getCol(x, y)) {
-				ai->danger = j;
-				break;
-      }
-    }
-    if(ai->danger != 0) break;
-  }
+	/* first, check if we are in danger, or if we should consider
+		 turning once in a while
+	 */
 
-  if(ai->danger != 0 || ai->moves > max_moves[level] * game2->rules.grid_size) {
-    ai->moves = 0;
-
-    /* figure out how far it's to either side */
-    for(i = 1; i < MAX_PROBE_DIST; i++) {
-      setPos(data, &x, &y);
-      getDistPoint(LEFT(data->dir), i, &x, &y);
-      if(getCol(x, y)) {
-				ldist = i;
-				break;
-      } else { ldist = i; }
-    }
-    for(i = 1; i < MAX_PROBE_DIST; i++) {
-      setPos(data, &x, &y);
-      getDistPoint(RIGHT(data->dir), i, &x, &y);
-      if(getCol(x, y)) {
-				rdist = i;
-				break;
-      } else { rdist = i; }
-    }
-    /* decide where to turn */
-    if(ai->danger > rdist && ai->danger > ldist) {
-      ai->danger--;
-      return;
-    } else if(rdist > ldist && ai->tdiff > -spiral[level] ) {
-      createEvent(player, EVENT_TURN_RIGHT);
-      ai->tdiff--;
-    } else if(rdist < ldist && ai->tdiff < spiral[level] ) {
-      createEvent(player, EVENT_TURN_LEFT);
-      ai->tdiff++;
-    } else {
-      if(ai->tdiff > 0) { 
-				createEvent(player, EVENT_TURN_RIGHT);
-				ai->tdiff--; }
-      else { 
-				createEvent(player, EVENT_TURN_LEFT);
-				ai->tdiff++; }
-    }
-    ai->danger = 0;
-    ai->lasttime = game2->time.current;
-  }
+  if(front > critical[level] * game2->rules.grid_size &&
+		 segment2_Length(data->trails + data->trailOffset) <
+		 maxSegLength[level] * game2->rules.grid_size)
+		return;
+		
+	// printf("%.2f, %.2f, %.2f\n", front, left, right);
+	/* decide where to turn */
+	if(front > right && front > left) { 
+		return; /* no way out */
+	} else if(left > rlDelta[level] &&
+						abs(right - left) < rlDelta[level] &&
+						backleft > left) {
+		createEvent(player, EVENT_TURN_LEFT);
+		ai->tdiff++;
+	} else if(right > left && ai->tdiff > -spiral[level] ) {
+		createEvent(player, EVENT_TURN_RIGHT);
+		ai->tdiff--;
+		// printf("turn right\n");
+	} else if(right < left && ai->tdiff < spiral[level] ) {
+		createEvent(player, EVENT_TURN_LEFT);
+		ai->tdiff++;
+		// printf("turn left\n");
+	} else {
+		printf("tdiff decision\n");
+		if(ai->tdiff > 0) { 
+			createEvent(player, EVENT_TURN_RIGHT);
+			ai->tdiff--; 
+			// printf("turn right\n");
+		} else { 
+			createEvent(player, EVENT_TURN_LEFT);
+			ai->tdiff++; 
+			// printf("turn left\n");
+		}
+	}
+	ai->lasttime = game2->time.current;
 }
-
-/* new AI code */
-
-#define DIR_LEFT 1
-#define DIR_RIGHT 2
-#define DIR_FRONT 4
-#define DIR_BACK 8
-
-static void getXYdir(int dir, int *x, int *y) {
-  int tx = 0, ty = 0;
-  if(dir & DIR_FRONT) { tx = *x; ty = *y; }
-  if(dir & DIR_BACK) { tx = -*x; ty = -*y; }
-  if(dir & DIR_LEFT) { tx += -*y; ty += *x; }
-  if(dir & DIR_RIGHT) { tx += *y; ty += -*x; }
-  *x = tx; *y = ty;
-}
-
-static int freeway2(const Data* data, int xdir, int ydir, int c) {
-	int x, y;
-	x = floorf(data->posx);
-	y = floorf(data->posy);
-	
-  if(xdir == 0 && ydir == 0) {
-    fprintf(stderr, "bug: xdir == ydir == 0\n");
-    return 0;
-  }
-
-	
-  while(getCol(x + xdir * c, 
-							 y + ydir * c) == 0) c++;
-  return c;
-}
-
-/*
-  if FRONT < 0.92063 go straight
-  else if 0.92063 >= REAR_RIGHT turn left
-  else if LEFT < RIGHT turn right
-  else turn left
-*/
-
-#define MIN_FREE 1
-#define MIN_TURN 5
-#define CRIT_F 0.92063f
-
-static int min_turn[] = { 1, 1, 15, 6 };
-
-/* level 2 and better */
-
-void doComputer2(int player, int target) {
-  /* target is ignored */
-  Data *data;
-  Player *me;
-  AI *ai;
-  int level;
-  int front, rear_right, left, right;
-  int dx, dy;
-	int x, y;
-  float critical;
-
-  me = &(game->player[ player ]);
-  if(me->ai == NULL) {
-    printf("This player has no AI data!\n");
-    return;
-  }
-  ai = me->ai;
-  level = game2->settingsCache.ai_level;
-  data = me->data;
-	x = floorf(data->posx);
-	y = floorf(data->posy);
-  if(abs(x - ai->lastx) < min_turn[level] &&
-     abs(y - ai->lasty) < min_turn[level]) {
-    /* fprintf(stderr, "too early\n"); */
-    return;
-  }
-
-  critical = (1 - CRIT_F) * game2->rules.grid_size;
-  dx = dirsX[ data->dir ];
-  dy = dirsY[ data->dir ];
-  getXYdir(DIR_FRONT, &dx, &dy);
-  front = freeway2(data, dx, dy, MIN_FREE);
-
-  dx = dirsX[ data->dir ];
-  dy = dirsY[ data->dir ];
-  getXYdir(DIR_BACK | DIR_RIGHT, &dx, &dy);
-  rear_right = freeway2(data, dx, dy, MIN_FREE);
-
-  dx = dirsX[ data->dir ];
-  dy = dirsY[ data->dir ];
-  getXYdir(DIR_RIGHT, &dx, &dy);
-  right = freeway2(data, dx, dy, MIN_FREE);
-
-  dx = dirsX[ data->dir ];
-  dy = dirsY[ data->dir ];
-  getXYdir(DIR_LEFT, &dx, &dy);
-  left = freeway2(data, dx, dy, MIN_FREE);
-
-  /*
-    fprintf(stderr, "-------------------------\n");
-    fprintf(stderr, "front space %d\n", front);
-    fprintf(stderr, "rear_right space %d\n", rear_right);
-    fprintf(stderr, "right space %d\n", right);
-    fprintf(stderr, "left space %d\n", left);
-  */
-
-  if(front < critical && (left > front || right > front) ) {
-    if(left < right) {
-      /* turn right */
-      createEvent(player, EVENT_TURN_RIGHT);
-    } else {
-      /* turn left */
-      createEvent(player, EVENT_TURN_LEFT);
-    }
-    ai->lastx = x;
-    ai->lasty = y;
-  }
-}
-
