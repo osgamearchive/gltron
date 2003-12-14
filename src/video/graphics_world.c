8dc67dda4aabe96f20e5d605489a394e6ad197d0
Andreas Umbach <umbach@gmail.com>
2003-12-14 16:50:55 +0000

    semi transparent floor (for reflections); fixed indenting

--- src/video/graphics_world.c
+++ src/video/graphics_world.c
@@ -82,32 +82,32 @@ int drawFloorTextured(int grid_size, GLuint texture) {
   glEnable(GL_TEXTURE_2D);
   glBindTexture(GL_TEXTURE_2D, texture);
 
-    /* there are some strange clipping artefacts on some renderers */
-    /* try subdividing things... */
+	/* there are some strange clipping artefacts on some renderers */
+	/* try subdividing things... */
 
-    glColor4f(1.0, 1.0, 1.0, 1.0);
-    l = grid_size / 4;
-    t = l / 12;
+	glColor4f(1.0, 1.0, 1.0, .5); // for reflections
+	l = grid_size / 4;
+	t = l / 12;
     
-    //  glBegin(GL_QUADS);
-    for (i = 0; i < grid_size; i += l) {
-      glBegin(GL_QUADS);
-      for (j = 0; j < grid_size; j += l) {
-        glTexCoord2i(0, 0);
-        glVertex2i(i, j);
-        glTexCoord2i(t, 0);
-        glVertex2i(i + l, j);
-        glTexCoord2i(t, t);
-        glVertex2i(i + l, j + l);
-        glTexCoord2i(0, t);
-        glVertex2i(i, j + l);
-      }
-      glEnd();
-    }
-    // glEnd();
+	//  glBegin(GL_QUADS);
+	for (i = 0; i < grid_size; i += l) {
+		glBegin(GL_QUADS);
+		for (j = 0; j < grid_size; j += l) {
+			glTexCoord2i(0, 0);
+			glVertex2i(i, j);
+			glTexCoord2i(t, 0);
+			glVertex2i(i + l, j);
+			glTexCoord2i(t, t);
+			glVertex2i(i + l, j + l);
+			glTexCoord2i(0, t);
+			glVertex2i(i, j + l);
+		}
+		glEnd();
+	}
+	// glEnd();
    
-    glDisable(GL_TEXTURE_2D);
-    return grid_size * grid_size;
+	glDisable(GL_TEXTURE_2D);
+	return grid_size * grid_size;
 }
 
 void drawFloorGrid(int grid_size,  int line_spacing, 
