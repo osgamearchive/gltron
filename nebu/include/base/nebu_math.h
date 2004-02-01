222671a24ce68db8e9a035e8396988227052ad97
Andreas Umbach <umbach@gmail.com>
2004-02-01 17:44:32 +0000

    new file

---
+++ nebu/include/base/nebu_math.h
@@ -0,0 +1,15 @@
+#ifndef NEBU_MATH_H
+#define NEBU_MATH_H
+
+#include <math.h>
+#include <limits.h>
+
+#ifndef FLT_MAX
+#define FLT_MAX 3.402823466e+38F
+#endif
+
+#ifndef FLT_MIN
+#define FLT_MIN 1.175494351e-38F
+#endif
+
+#endif
