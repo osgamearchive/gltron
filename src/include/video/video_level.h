ca1bcc0945a3478121d4582bd155b24745052239
Andreas Umbach <umbach@gmail.com>
2004-02-08 20:33:28 +0000

    added shader stuff

--- src/include/video/video_level.h
+++ src/include/video/video_level.h
@@ -5,14 +5,23 @@
 #include "video/nebu_mesh.h"
 
 typedef struct {
+	int lit;
+	int diffuse_texture_id;
+} video_level_shader;
+
+typedef struct {
 	int scalable;
 
 	nebu_Mesh *floor;
 	nebu_Mesh *arena;
+
+	video_level_shader floor_shader;
+	video_level_shader arena_shader;
 } video_level;
 
 void video_FreeLevel(video_level *l);
 video_level* video_CreateLevel(void);
 void video_ScaleLevel(video_level *l, float fSize);
-
+void video_Shader_Setup(video_level_shader *shader);
+void video_Shader_Cleanup(video_level_shader *shader);
 #endif
