52bca4423757eede263d04d890144dc6386a3bd6
Andreas Umbach <umbach@gmail.com>
2004-04-30 12:44:32 +0000

    added c_reloadLevel(), c_loadDirectory()

--- src/game/scripting_interface.c
+++ src/game/scripting_interface.c
@@ -1,6 +1,7 @@
 #include "game/gltron.h"
 #include "base/switchCallbacks.h"
 #include "scripting/nebu_scripting.h"
+#include "filesystem/path.h"
 
 #include "audio/audio.h"
 
@@ -14,7 +15,8 @@ int c_quitGame(lua_State *L) {
 }
 
 int c_resetGame(lua_State *L) {
-  initData();
+  game_ResetData();
+	video_ResetData();
   return 0;
 }
 
@@ -62,7 +64,8 @@ int c_update_audio_volume(lua_State *L) {
 
 int c_startGame(lua_State *L) { 
   game2->mode = GAME_SINGLE;
-  initData();
+	game_ResetData();
+	video_ResetData();
   changeDisplay(-1);
 	SystemExitLoop(RETURN_GAME_LAUNCH);
   return 0;
@@ -75,6 +78,15 @@ int c_reloadTrack(lua_State *L) {
 
 int c_reloadArtpack(lua_State *L) {
   reloadArt();
+  return 0;
+}
+
+int c_reloadLevel(lua_State *L) {
+  loadLevel();
+	reloadArt();
+	initGameLevel();
+	game_ResetData();
+	video_ResetData(); // already called by reloadArt()
   return 0;
 }
   
@@ -116,6 +128,38 @@ int c_SystemMainLoop(lua_State *L) {
 	return 1;
 }
 
+int c_loadDirectory(lua_State *L) {
+	int dir;
+	char *dirPath;
+	nebu_List *files, *p;
+	int nFiles = 0;
+
+	// load directory enum from stack
+	int top = lua_gettop(L); // number of arguments
+	if(top != 1) {
+		// wrong number of arguments
+		lua_error(L, "wrong number of arguments for function "
+							"c_loadDirectory: should be 1\n");
+	}
+	if(!lua_isnumber(L, -1)) {
+		lua_error(L, "number  expected for arg1 to function "
+							"c_loadDirecotry");
+	}
+	dir = lua_tonumber(L, -1);
+
+	dirPath = getDirectory(dir); // PATH_ART or PATH_LEVEL or PATH_MUSIC
+	files = readDirectoryContents(dirPath, NULL);
+
+	lua_newtable(L);
+	for(p = files; p->next; p = p->next) {
+		lua_pushstring(L, p->data);
+		lua_rawseti(L, -2, nFiles + 1);
+		nFiles++;
+	}
+	/* FIXME: when does the list get freed */
+	return 1;
+}
+
 void init_c_interface(void) {
   scripting_Register("c_quitGame", c_quitGame);
   scripting_Register("c_resetGame", c_resetGame);
@@ -127,10 +171,11 @@ void init_c_interface(void) {
   scripting_Register("c_startGame", c_startGame);
   scripting_Register("c_reloadTrack", c_reloadTrack);
   scripting_Register("c_reloadArtpack", c_reloadArtpack);
+  scripting_Register("c_reloadLevel", c_reloadLevel);
   scripting_Register("c_configureKeyboard", c_configureKeyboard);
   scripting_Register("c_getKeyName", c_getKeyName);
 	scripting_Register("c_timedemo", c_timedemo);
-	
+	scripting_Register("c_loadDirectory", c_loadDirectory);
 	scripting_Register("SystemMainLoop", c_SystemMainLoop);
 	scripting_Register("SetCallback", c_SetCallback);
 }
