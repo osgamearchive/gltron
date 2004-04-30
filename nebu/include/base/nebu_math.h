e0124524d47051bcc08a1989a0072a5ecad66138
Andreas Umbach <umbach@gmail.com>
2004-04-30 12:43:18 +0000

    added PI, and win32 M_PI and sinf/cosf etc. defines

--- nebu/include/base/nebu_math.h
+++ nebu/include/base/nebu_math.h
@@ -4,6 +4,25 @@
 #include <math.h>
 #include <limits.h>
 
+#ifndef M_PI
+#define M_PI 3.141592654
+#endif
+
+#define PI ((float) M_PI)
+
+#ifdef WIN32
+#define floorf (float)floor
+#define fabsf (float)fabs
+#define sqrtf (float)sqrt
+#define cosf (float) cos
+#define sinf (float) sin
+#define acosf (float) acos
+#define tanf (float) tan
+#endif
+
+#define nebu_cosf_deg(X)	cosf( (X) * M_PI/180.0 )
+#define nebu_sinf_deg(X)	sinf( (X) * M_PI/180.0 )
+
 #ifndef FLT_MAX
 #define FLT_MAX 3.402823466e+38F
 #endif
