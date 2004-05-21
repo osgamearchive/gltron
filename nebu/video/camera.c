3267cc77970c51a543c3a98127a429e53f243c9b
Andreas Umbach <umbach@gmail.com>
2004-05-21 07:52:16 +0000

    added nebu_Camera_RotateAroundTarget (not debugged yet)

--- nebu/video/camera.c
+++ nebu/video/camera.c
@@ -1,4 +1,7 @@
 #include "video/nebu_camera.h"
+#include "video/nebu_gl.h"
+#include "base/nebu_math.h"
+#include "base/nebu_matrix.h"
 
 static void doLookAt(float *cam, float *target, float *up) {
   float m[16];
@@ -23,6 +26,50 @@ static void doLookAt(float *cam, float *target, float *up) {
   glTranslatef( -cam[0], -cam[1], -cam[2]);
 }
 
-nebu_Camera_LookAt(nebu_Camera *pCamera) {
+void nebu_Camera_LookAt(nebu_Camera *pCamera) {
 	doLookAt(pCamera->vEye.v, pCamera->vLookAt.v , pCamera->vUp.v);
+}
+
+void nebu_Camera_RotateAroundTarget(nebu_Camera *pCamera,
+																		int dx, int dy) {
+	// adjust up vector, so that it is orthogonal to
+	// view direction
+	vec3 vDiff, vView, vRight, vUp;
+	vec3 vdx, vdy;
+	
+	vec3_Sub(&vDiff, &pCamera->vLookAt, &pCamera->vEye);
+	vec3_Normalize(&vView, &vDiff);
+	vec3_Cross(&vRight, &pCamera->vUp, &vView);
+	vec3_Normalize(&vRight, &vRight);
+	vec3_Cross(&vUp, &vView, &vRight);
+	vec3_Normalize(&vUp, &vUp);
+	
+	// horizontal movement (dx):
+	if(dx == 0) {
+		vec3_Zero(&vdx);
+	} else {
+		// rotate eye point around up vector through lookAt point
+		vec3 v = vDiff;
+		float fAngle = dx * 2 * M_PI / 360.0;
+		matrix matRotation;
+		matrixRotationAxis(&matRotation, fAngle, &vUp);
+		vec3Transform(&v, &vDiff, &matRotation);
+		vec3_Sub(&vdx, &v, &vDiff);
+	}
+	// vertical movement (dy):
+	if(dy == 0) {
+		vec3_Zero(&vdy);
+	} else {
+		// rotate eye point around up vector through lookAt point
+		vec3 v = vDiff;
+		float fAngle = dx * 2 * M_PI / 360.0;
+		matrix matRotation;
+		matrixRotationAxis(&matRotation, fAngle, &vRight);
+		vec3Transform(&v, &vDiff, &matRotation);
+		vec3_Sub(&vdx, &v, &vDiff);
+	}
+
+	// add relative movements to camera position
+	vec3_Add(&pCamera->vEye, &pCamera->vEye, &vdx);
+	vec3_Add(&pCamera->vEye, &pCamera->vEye, &vdy);
 }
