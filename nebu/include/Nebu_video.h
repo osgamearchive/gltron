dbf5844635138a9896619cc86638a5be92b61634
Andreas Umbach <umbach@gmail.com>
2004-05-21 07:51:22 +0000

    moved texture/surface stuff to base

--- nebu/include/Nebu_video.h
+++ nebu/include/Nebu_video.h
@@ -4,7 +4,6 @@
 #include "video/nebu_video_types.h"
 #include "video/nebu_console.h"
 #include "video/nebu_light.h"
-#include "video/nebu_png_texture.h"
 #include "video/nebu_quad.h"
 #include "video/nebu_renderer_gl.h"
 #include "video/nebu_video_system.h"
