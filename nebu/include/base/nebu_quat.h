efb9bf0b1c88692b4521388c975c2dd25caaca1e
Andreas Umbach <umbach@gmail.com>
2004-05-21 07:51:31 +0000

    removed typedef quat

--- nebu/include/base/nebu_quat.h
+++ nebu/include/base/nebu_quat.h
@@ -5,11 +5,9 @@
 
 typedef struct {
   float x,y,z,w;
-} quat;
-
-typedef quat nebu_Quat;
+} nebu_Quat;
 
 nebu_Matrix4D quat_RotationMatrix(const nebu_Quat *quat);
-quat quat_FromLookAt(const vec3 *vLookAt, const vec3 *vUp);
+nebu_Quat quat_FromLookAt(const vec3 *vLookAt, const vec3 *vUp);
 
 #endif
