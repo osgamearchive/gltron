bb715fc0d956d43e37ee4959d78ff32cb567e028
Andreas Umbach <umbach@gmail.com>
2004-05-29 17:15:34 +0000

    renamed vec3Transform to vec3_Transform

--- nebu/include/base/nebu_matrix.h
+++ nebu/include/base/nebu_matrix.h
@@ -8,8 +8,9 @@ typedef struct {
 } matrix;
 typedef matrix nebu_Matrix4D;
 
-vec4* vec4Transform(vec4* pOut, const vec4* pV, const matrix *pM);
-vec3* vec3Transform(vec3* pOut, const vec3* pV, const matrix *pM);
+vec4* vec4_Transform(vec4* pOut, const vec4* pV, const matrix *pM);
+vec3* vec3_Transform(vec3* pOut, const vec3* pV, const matrix *pM);
+
 float matrixCofactor(const matrix *pM, int cf_row, int cf_col);
 float matrixDeterminant(const matrix *pM);
 matrix* matrixIdentity(matrix *pOut);
@@ -17,8 +18,6 @@ matrix* matrixInverse(matrix *pOut, float* pDet, const matrix *pM);
 matrix* matrixMultiply(matrix *pOut, const matrix *pM1, const matrix *pM2);
 matrix* matrixTranspose(matrix *pOut, const matrix *pM);
 matrix* matrixAdjoint(matrix *pOut, float* pDet, const matrix *pM);
-
-vec4* vec4Transform(vec4 *pOut, const vec4 *pV, const matrix *pM);
 
 matrix* matrixRotationAxis(matrix *pOut, float fAngle, const vec3 *vAxis);
 matrix* matrixTranslation(matrix *pOut, const vec3 *vTranslation);
