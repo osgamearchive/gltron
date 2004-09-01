e00ad6dd0bc75f45566465c9e0e5af651b76678f
Andreas Umbach <umbach@gmail.com>
2004-09-01 08:04:26 +0000

    new vertex format flags for multi-texturing

--- nebu/include/video/nebu_mesh.h
+++ nebu/include/video/nebu_mesh.h
@@ -1,11 +1,28 @@
 #ifndef NEBU_MESH_H
 #define NEBU_MESH_H
 
+enum {
+	NEBU_MESH_POSITION    = 0x0001,
+	NEBU_MESH_NORMAL      = 0x0002,
+	NEBU_MESH_TEXCOORD0   = 0x0004,
+	NEBU_MESH_TEXCOORD1   = 0x0008,
+	NEBU_MESH_TEXCOORD2   = 0x0010,
+	NEBU_MESH_TEXCOORD3   = 0x0020,
+	NEBU_MESH_TEXCOORD4   = 0x0040,
+	NEBU_MESH_TEXCOORD5   = 0x0080,
+	NEBU_MESH_TEXCOORD6   = 0x0100,
+	NEBU_MESH_TEXCOORD7   = 0x0200,
+	NEBU_MESH_TEXCOORD_MAXCOUNT = 8,
+
+	NEBU_MESH_FLAGS = 10
+};
+
+
 typedef struct {
 	int nVertices;
 	float *pVertices;
 	float *pNormals;
-	float *pTexCoords;
+	float *pTexCoords[NEBU_MESH_TEXCOORD_MAXCOUNT];
 	int vertexformat;
 
 	int nTriangles;
@@ -13,12 +30,6 @@ typedef struct {
 
 } nebu_Mesh;
 
-enum {
-	NEBU_MESH_POSITION    = 0x0001,
-	NEBU_MESH_NORMAL      = 0x0002,
-	NEBU_MESH_TEXCOORD    = 0x0004,
-	NEBU_MESH_FLAGS = 3
-};
 
 nebu_Mesh* nebu_Mesh_Create(int flags, int nVertices, int nTriangles);
 nebu_Mesh* nebu_Mesh_Clone(int flags, nebu_Mesh *pMesh);
