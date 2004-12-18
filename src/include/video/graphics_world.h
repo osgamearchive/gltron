29a5f5f65f37c780437fd5e7099209060cb0fb6b
Andreas Umbach <umbach@gmail.com>
2004-12-18 15:56:29 +0000

    removed unused prototypes

--- src/include/video/graphics_world.h
+++ src/include/video/graphics_world.h
@@ -2,8 +2,5 @@
 #define GRAPHICS_WORLD_H
 
 void drawWalls(void);
-int drawFloorTextured(int grid_size, GLuint texture);
-void drawFloorGrid(int grid_size,  int line_spacing,
-                   float line_color[4], float square_color[4]);
 
 #endif
