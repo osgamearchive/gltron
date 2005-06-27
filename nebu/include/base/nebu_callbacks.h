ee2349f55f995df8a5da40345d73fab252e92b81
Andreas Umbach <umbach@gmail.com>
2005-06-27 06:32:13 +0000

    fixed indenting

--- nebu/include/base/nebu_callbacks.h
+++ nebu/include/base/nebu_callbacks.h
@@ -2,13 +2,13 @@
 #define NEBU_CALLBACKS_H
 
 typedef struct Callbacks {
-  void (*display)(void);
-  void (*idle)(void);
-  void (*keyboard)(int, int, int, int);
-  void (*init)(void);
-  void (*exit)(void);
-  void (*mouse)(int, int, int, int);
-  void (*mouseMotion)(int, int);
+	void (*display)(void);
+	void (*idle)(void);
+	void (*keyboard)(int, int, int, int);
+	void (*init)(void);
+	void (*exit)(void);
+	void (*mouse)(int, int, int, int);
+	void (*mouseMotion)(int, int);
 	char *name;
 } Callbacks;
 
