efa30befe3254ba98fe1fa2f7db57ffac07027b0
Andreas Umbach <umbach@gmail.com>
2006-10-08 07:58:20 +0000

    resources can have meta data now

--- src/include/game/resource.h
+++ src/include/game/resource.h
@@ -19,9 +19,11 @@ typedef enum EResourceUserTypes {
 
 void resource_Release(int token);
 void resource_ReleaseAll(void);
+void resource_ReleaseType(int type);
 void resource_FreeAll(void);
 void resource_Free(int token);
 void* resource_Get(int token, int type);
 int resource_GetToken(char *filename, int type);
+int resource_GetTokenMeta(char *filename, int type, void *metadata, int metasize);
 
 #endif /* RESOURCE_H */
