e2ae84e51a3079d1235089593491de9a0372281a
Andreas Umbach <umbach@gmail.com>
2006-10-08 07:58:01 +0000

    new bounidng box, material types

--- src/include/video/model.h
+++ src/include/video/model.h
@@ -1,15 +1,16 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-#define MAX_VERTICES 60000
-#define MAX_FACES 120000
-#define MAX_NORMALS 60000
-#define MAX_MATERIALS 200
-
-#define BUF_SIZE 120
-
 #include "base/nebu_vector.h"
 #include "video/nebu_mesh.h"
+
+typedef enum eMaterialType
+{
+	eMT_Ambient		= 0x01,
+	eMT_Diffuse		= 0x02,
+	eMT_Specular	= 0x04,
+	eMT_Shininess	= 0x08
+} eMaterialType;
 
 typedef struct gltron_Mesh_Material {
   float ambient[4];
@@ -17,14 +18,8 @@ typedef struct gltron_Mesh_Material {
   float specular[4];
   float shininess;
   char *name;
-  char *map_diffuse;
+  int flags;
 } gltron_Mesh_Material;
-
-typedef struct gltron_Mesh_BoundingBox {
-  vec3 vMin;
-  vec3 vSize;
-  float fRadius;
-} gltron_Mesh_BoundingBox;
 
 typedef struct gltron_Mesh {
 	/*
@@ -34,11 +29,13 @@ typedef struct gltron_Mesh {
 	*/
 	nebu_Mesh_VB *pVB;
 	nebu_Mesh_ShadowInfo *pSI;
-	unsigned short **ppIndices;
-	int *pnFaces;
+	// unsigned short **ppIndices;
+	// int *pnFaces;
+	nebu_Mesh_IB **ppIB;
 	int nMaterials;
-	gltron_Mesh_Material* pMaterials;
-	gltron_Mesh_BoundingBox BBox;
+	gltron_Mesh_Material** ppMaterials;
+	box3 BBox;
+	int bIsBBoxValid;
 } gltron_Mesh;
 
 typedef enum gltron_MeshType {
@@ -46,10 +43,18 @@ typedef enum gltron_MeshType {
   QUAD_MESH
 } gltron_MeshType;
     
-gltron_Mesh* gltron_Mesh_LoadFromFile(const char* filename, gltron_MeshType type);
+gltron_Mesh* gltron_Mesh_Create(int vertexformat, int nVertices, int *pnPrimitives, int nMaterials);
+gltron_Mesh* gltron_Mesh_LoadFromFile(const char* pFilename, gltron_MeshType type);
+gltron_Mesh* gltron_Mesh_LoadFromMesh(nebu_Mesh *pMesh, gltron_Mesh_Material *pMaterial);
+gltron_Mesh_Material* gltron_Mesh_Material_Create(const char *pName, int flags);
+void gltron_Mesh_Material_Free(gltron_Mesh_Material *pMaterial);
+void gltron_Mesh_Material_Copy(gltron_Mesh_Material *pDst, const gltron_Mesh_Material *pSrc);
+void gltron_Mesh_Material_Set(gltron_Mesh_Material *pMat);
+
 void gltron_Mesh_Draw(gltron_Mesh* pMesh, gltron_MeshType type);
 void gltron_Mesh_DrawExplosion(gltron_Mesh *pMesh, float fRadius);
 void gltron_Mesh_Free(gltron_Mesh* pMesh);
+void gltron_Mesh_Scale(gltron_Mesh* pMesh, float fScale);
 
 typedef enum ColorType {
   eAmbient = 0,
@@ -57,8 +62,8 @@ typedef enum ColorType {
   eSpecular
 } ColorType;
 
-void gltron_Mesh_SetMaterialAlpha(gltron_Mesh *pMesh, float fAlpha);
-void gltron_Mesh_SetMaterialColor(gltron_Mesh *pMesh, char *name, 
+void gltron_Mesh_Material_SetAlpha(gltron_Mesh *pMesh, float fAlpha);
+void gltron_Mesh_Material_SetColor(gltron_Mesh *pMesh, char *name, 
 	 ColorType eType, float pColor[4]);
 
 void gltron_Mesh_ComputeBBox(gltron_Mesh* pMesh);
