14a90c8be4e8a056f7aa2e64555cf7051d3578e8
Andreas Umbach <umbach@gmail.com>
2006-10-08 07:59:59 +0000

    resources can have meta-data now

--- src/game/resource.c
+++ src/game/resource.c
@@ -1,8 +1,8 @@
 #include "game/resource.h"
 #include "base/nebu_util.h"
 #include "video/nebu_font.h"
-
-#include <assert.h>
+#include "video/nebu_texture2d.h"
+#include "base/nebu_assert.h"
 #include <string.h>
 
 // TODO: move user type load functions somewhere else
@@ -12,32 +12,34 @@
 #include "base/nebu_debug_memory.h"
 
 typedef struct {
-	void *data;
-	char *filename;
-	int type;
-	int token;
-} Token;
+	void *data; // the loaded & ready-to-use resource
+	char *filename; // the resource location in the filesystem
+	void *metadata;	// optional metadata information for the resource
+	int metasize; // size of the metadata chunk
+	int type; // resource type identifier
+	int token; // the (unique) identifier for this resource
+} ResourceToken;
 
 static nebu_List *pTokenList = NULL;
 static int nextToken = 1;
 
-static Token* findToken(int token)
+static ResourceToken* findToken(int token)
 {
 	nebu_List *p;
-	assert(token);
-	assert(pTokenList);
+	nebu_assert(token);
+	nebu_assert(pTokenList);
 	for(p = pTokenList; p->next; p = p->next)
 	{
-		Token* pToken = (Token*) p->data;
+		ResourceToken* pToken = (ResourceToken*) p->data;
 		if(pToken->token == token)
 			return pToken;
 	}
 	return NULL;
 }
 
-static void release(Token *pToken)
+static void release(ResourceToken *pToken)
 {
-	assert(pToken);
+	nebu_assert(pToken);
 	if(!pToken->data)
 		return;
 
@@ -53,8 +55,11 @@ static void release(Token *pToken)
 	case eRT_Font:
 		nebu_Font_Free((nebu_Font*)pToken->data);
 		break;
+	case eRT_Texture:
+		nebu_Texture2D_Free((nebu_Texture2D*)pToken->data);
+		break;
 	default:
-		assert(0);
+		nebu_assert(0);
 		break;
 	}
 	pToken->data = NULL;
@@ -69,13 +74,15 @@ void resource_Free(int token)
 {
 	nebu_List *p;
 	nebu_List *pPrev = NULL;
-	assert(pTokenList);
+	nebu_assert(pTokenList);
 	for(p = pTokenList; p->next; p = p->next)
 	{
-		Token *pToken = (Token*)p->data;
+		ResourceToken *pToken = (ResourceToken*)p->data;
 		if(pToken->token == token)
 		{
 			release( pToken );
+			if(pToken->metadata)
+				free(pToken->metadata);
 			free(pToken->filename);
 			free(pToken);
 			nebu_List_RemoveAt(p, pPrev);
@@ -87,6 +94,20 @@ void resource_Free(int token)
 	}
 }
 
+void resource_ReleaseType(int type)
+{
+	nebu_List *p;
+	if(!pTokenList)
+		return;
+
+	for(p = pTokenList; p->next; p = p->next)
+	{
+		if(((ResourceToken*)p->data)->type == type)
+			release((ResourceToken*)p->data);
+	}
+
+}
+
 void resource_ReleaseAll(void)
 {
 	nebu_List *p;
@@ -95,7 +116,7 @@ void resource_ReleaseAll(void)
 
 	for(p = pTokenList; p->next; p = p->next)
 	{
-		release((Token*)p->data);
+		release((ResourceToken*)p->data);
 	}
 }
 
@@ -107,9 +128,15 @@ void resource_FreeAll(void)
 
 	for(p = pTokenList; p->next; p = p->next)
 	{
-		release((Token*)p->data);
-		free(((Token*)p->data)->filename);
-		free(p->data);
+		ResourceToken *pResource = (ResourceToken*)p->data;
+		nebu_assert(pResource);
+		release(pResource);
+		free(pResource->filename);
+		if(pResource->metadata)
+		{
+			free(pResource->metadata);
+		}
+		free(pResource);
 	}
 	nebu_List_Free(pTokenList);
 	pTokenList = NULL;
@@ -117,11 +144,11 @@ void resource_FreeAll(void)
 
 void* resource_Get(int token, int type)
 {
-	Token *pToken = findToken(token);
-	assert(pToken);
+	ResourceToken *pToken = findToken(token);
+	nebu_assert(pToken);
 	if(!pToken)
 		return NULL;
-	assert(pToken->type == type);
+	nebu_assert(pToken->type == type);
 	if(pToken->data)
 		return pToken->data;
 
@@ -139,24 +166,27 @@ void* resource_Get(int token, int type)
 	case eRT_Font:
 		pToken->data = nebu_Font_Load(pToken->filename, PATH_ART);
 		break;
+	case eRT_Texture:
+		pToken->data = nebu_Texture2D_Load(pToken->filename, PATH_ART, (nebu_Texture2D_meta*) pToken->metadata);
+		break;
 	default:
-		assert(0);
+		nebu_assert(0);
 		break;
 
 		// custom load function
 	}
-	assert(pToken->data);
+	nebu_assert(pToken->data);
 	return pToken->data;
 }
 
-static Token* findIdenticalToken(char *filename, int type)
+static ResourceToken* findIdenticalToken(char *filename, int type)
 {
 	nebu_List *p;
 	if(!pTokenList)
 		return NULL;
 	for(p = pTokenList; p->next; p = p->next)
 	{
-		Token *pToken = (Token*) p->data;
+		ResourceToken *pToken = (ResourceToken*) p->data;
 		if(pToken->type == type && 
 			strcmp(filename, pToken->filename) == 0)
 			return pToken;
@@ -166,15 +196,33 @@ static Token* findIdenticalToken(char *filename, int type)
 
 int resource_GetToken(char *filename, int type)
 {
-	Token *pToken;
+	return resource_GetTokenMeta(filename, type, NULL, 0);
+}
+
+int resource_GetTokenMeta(char *filename, int type, void *metadata, int metasize)
+{
+	ResourceToken *pToken;
 
 	if(filename == NULL || *filename == 0)
 		return 0;
 
-	assert(!findIdenticalToken(filename, type));
+	nebu_assert(!findIdenticalToken(filename, type));
 
-	pToken = (Token*)malloc(sizeof(Token));
+	pToken = (ResourceToken*)malloc(sizeof(ResourceToken));
 	pToken->data = NULL;
+	if(!metadata)
+	{
+		pToken->metadata = NULL;
+		pToken->metasize = 0;
+	}
+	else
+	{
+		nebu_assert(metasize);
+		pToken->metadata = malloc(metasize);
+		pToken->metasize = metasize;
+		memcpy(pToken->metadata, metadata, metasize);
+	}
+
 	pToken->filename = (char*) malloc(strlen(filename) + 1);
 	strcpy(pToken->filename, filename);
 	pToken->type = type;
