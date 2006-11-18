697dba808fc19fbac03bdd7c1a8b8ef50d62d268
Andreas Umbach <umbach@gmail.com>
2006-11-18 13:22:58 +0000

    added body for bs_close because gcc (OSX) complained

--- nebu/audio/music_rwop.c
+++ nebu/audio/music_rwop.c
@@ -5,11 +5,13 @@ SDL_RWops* bs_RWop;
 #define BUFSIZE 65536
 #define LOW_WATER 16384
 
-static int bs_read(SDL_RWops *context, void *ptr, int size, int maxnum) {
-  
+static int bs_read(SDL_RWops *context, void *ptr, int size, int maxnum)
+{
 }
 
-static int bs_close();
+static int bs_close()
+{
+}
 										
 SDL_RWops* bs_RWFromFile(const char *file) {
   bs_RWop = SDL_AllocRW();
