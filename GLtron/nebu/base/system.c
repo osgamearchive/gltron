60a7ab3333d6350ad6836ceefe4e7951e7d5b4bc
Andreas Umbach <umbach@gmail.com>
2014-10-28 08:50:09 +0100

    added stubs for SDL2 app events, fixed some whitespace

--- GLtron/nebu/base/system.c
+++ GLtron/nebu/base/system.c
@@ -123,31 +123,86 @@ void nebu_Time_FrameDelay(unsigned int delay)
 	// nebu_Time_SetCurrentFrameTime( nebu_Time_GetElapsed() );
 }
 
+#ifdef SDL2
+
+int nebu_HandleAppEvent(SDL_Event* event)
+{
+    switch(event->type)
+    {
+        case SDL_APP_TERMINATING:
+            /* Terminate the app.
+             Shut everything down before returning from this function.
+             */
+            return 0;
+        case SDL_APP_LOWMEMORY:
+            /* You will get this when your app is paused and iOS wants more memory.
+             Release as much memory as possible.
+             */
+            return 0;
+        case SDL_APP_WILLENTERBACKGROUND:
+            /* Prepare your app to go into the background.  Stop loops, etc.
+             This gets called when the user hits the home button, or gets a call.
+             */
+            return 0;
+        case SDL_APP_DIDENTERBACKGROUND:
+            /* This will get called if the user accepted whatever sent your app to the background.
+             If the user got a phone call and canceled it, you'll instead get an    SDL_APP_DIDENTERFOREGROUND event and restart your loops.
+             When you get this, you have 5 seconds to save all your state or the app will be terminated.
+             Your app is NOT active at this point.
+             */
+            return 0;
+        case SDL_APP_WILLENTERFOREGROUND:
+            /* This call happens when your app is coming back to the foreground.
+             Restore all your state here.
+             */
+            return 0;
+        case SDL_APP_DIDENTERFOREGROUND:
+            /* Restart your loops here.
+             Your app is interactive and getting CPU again. */
+            return 0;
+        default:
+            // not supposed to happen
+            nebu_assert(0);
+            return 0;
+    }
+}
+#endif
+
 int nebu_System_MainLoop() {
 	SDL_Event event;
 
 	return_code = -1;
 	while(return_code == -1) {
 		while(SDL_PollEvent(&event)) {
-			switch(event.type) {
-			case SDL_KEYDOWN:
-			case SDL_KEYUP:
-			case SDL_JOYAXISMOTION:
-			case SDL_JOYBUTTONDOWN:
-			case SDL_JOYBUTTONUP:
-			case SDL_MOUSEBUTTONUP:
-			case SDL_MOUSEBUTTONDOWN:
-			case SDL_MOUSEMOTION:
-				nebu_Intern_HandleInput(&event);
-			break;
-			case SDL_QUIT:
-				nebu_System_Exit(); // shut down
-				nebu_System_ExitLoop(0); // exit mainloop
-			break;
-			default:
-				/* ignore event */
-			break;
-			}
+            switch(event.type) {
+                case SDL_KEYDOWN:
+                case SDL_KEYUP:
+                case SDL_JOYAXISMOTION:
+                case SDL_JOYBUTTONDOWN:
+                case SDL_JOYBUTTONUP:
+                case SDL_MOUSEBUTTONUP:
+                case SDL_MOUSEBUTTONDOWN:
+                case SDL_MOUSEMOTION:
+                    nebu_Intern_HandleInput(&event);
+                    break;
+                case SDL_QUIT:
+                    nebu_System_Exit(); // shut down
+                    nebu_System_ExitLoop(0); // exit mainloop
+                    break;
+#ifdef SDL2
+                case SDL_APP_TERMINATING:
+                case SDL_APP_LOWMEMORY:
+                case SDL_APP_WILLENTERBACKGROUND:
+                case SDL_APP_DIDENTERBACKGROUND:
+                case SDL_APP_WILLENTERFOREGROUND:
+                case SDL_APP_DIDENTERFOREGROUND:
+                    nebu_HandleAppEvent(&event);
+                    break;
+#endif
+                default:
+                    /* ignore event */
+                    break;
+            }
 		}
 		if(current && current->display && redisplay) {
 			current->display();
